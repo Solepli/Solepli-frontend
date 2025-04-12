fc22efd60999729a6bf0fd2b1b957fb1598e0852
sky121016 <sky121016@naver.com>
2025-04-12 15:44:06 +0900

    [file] 프로젝트명 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import PreviewContentList from "./components/BottomSheet/PreviewContentList";
 
 function App() {
 
   return (
     <>
-    <PreviewContentList/>
+
     </>
   );
 }
