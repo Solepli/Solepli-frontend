e54dabb92de256046e9d3487d6846353ecc5e90f
sky121016 <sky121016@naver.com>
2025-04-13 20:31:15 +0900

    feat/#6 공유, X 버튼 아이콘 추가하기

---
+++ src/components/BottomSheet/DetailContent.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const DetailContent:React.FC = () => {
+  return (
+    <div>
+        {/* ContentTitle */}
+
+        {/* ReviewRange */}
+
+        {/* tags */}
+
+        {/* ReviewPhotoList */}
+        
+
+    </div>
+  )
+}
+
+export default DetailContent
