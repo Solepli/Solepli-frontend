0d6a3d882b6b224d4438fed704a58f010a29e5ce
sky121016 <sky121016@naver.com>
2025-04-13 20:29:58 +0900

    feat/#6 SolmarkChip 컴포넌트 다양화

--- src/components/BottomSheet/SolmarkChip.tsx
+++ src/components/BottomSheet/SolmarkChip.tsx
@@ -4,20 +4,46 @@ import { useState } from 'react';
 import heart from '../../assets/heart.svg';
 import heartFill from '../../assets/heartFill.svg';
 
-const SolmarkChip: React.FC = () => {
+interface SolmarkChipProps {
+  label: boolean;
+}
+
+const SolmarkChip: React.FC<SolmarkChipProps> = ({ label }) => {
   const [isSolmark, setIsSolmark] = useState<boolean>(false);
 
   const handleClick = () => {
     setIsSolmark((prev) => !prev);
   };
 
+  const gradient = 'bg-linear-120 from-gray-400 via-gray-600 to-gray-400 border-gray-600';
+
   return (
     <div onClick={handleClick}>
-      <div
-        className={`w-24 h-24 border rounded-lg flex justify-center items-center 
-            ${isSolmark ? 'bg-conic-45 from-gray-600 from-10% via-gray-400 via-70% to-gray-600 border-gray-600' : 'border-gray-400'}`}>
-        <img src={isSolmark ? heartFill : heart} alt='' className='w-24 h-24' />
-      </div>
+      {label ? (
+        <div
+          className={`flex w-58 h-32 p-2 border rounded-lg justify-center items-center 
+        ${isSolmark ? gradient : 'border-gray-400'}`}>
+          <p
+            className={`w-30 text-center text-xs ${isSolmark ? 'text-white' : 'text-gray-400'}`}>
+            1
+          </p>
+          <img
+            src={isSolmark ? heartFill : heart}
+            alt=''
+            className='w-24 h-24'
+          />
+        </div>
+      ) : (
+        <div
+          className={`flex w-24 h-24 border rounded-lg justify-center items-center 
+        ${isSolmark ? gradient : 'border-gray-400'}`}>
+          <img
+            src={isSolmark ? heartFill : heart}
+            alt=''
+            className='w-24 h-24'
+          />
+        </div>
+      )}
     </div>
   );
 };
