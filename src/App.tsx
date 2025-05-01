d0ade95e0c6282511f46f37bc38ac79bd5424719
sky121016 <sky121016@naver.com>
2025-05-01 14:13:53 +0900

    [feat/#6] App.js 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import DetailContent from "./components/BottomSheet/DetailContent";
-import PreviewContentList from "./components/BottomSheet/PreviewContentList";
 
 function App() {
   
   return (
     <>
-    {/* <ContentTitle place={place} property="detail"/> */}
-    <DetailContent />
-    <PreviewContentList/>
+    
     </>
   );
 }
