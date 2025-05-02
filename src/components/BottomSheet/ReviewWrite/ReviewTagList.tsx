8c3d71ba1b66780c2592c1f5654b58c18ffa499d
ion <delion.dev@gmail.com>
2025-05-02 15:19:47 +0900

    [feat/#7] ReviewTag, ReviewTagList 컴포넌트 작성

---
+++ src/components/BottomSheet/ReviewWrite/ReviewTagList.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReviewTag from './ReviewTag';
+import { Tag } from '../../../types';
+
+interface ReviewTagProps {
+  title: string;
+  tag: Tag[];
+}
+
+const ReviewTagList: React.FC<ReviewTagProps> = ({ title, tag }) => {
+  return (
+    <div className='flex flex-col gap-[6px] pt-[32px] px-[16px]'>
+      <div className='flex flex-row items-center justify-start py-4'>
+        <div className="text-[12px] leading-[120%] tracking-[-0.18px] font-['Pretendard'] font-[600] text-[#4f4f4f] whitespace-nowrap">
+          {title}
+        </div>
+      </div>
+      <div className='self-stretch flex flex-wrap items-center justify-start gap-0'>
+        {tag.map(({ id, text }) => {
+          return (
+            <div key={id} className='w-1/3'>
+              <ReviewTag text={text} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ReviewTagList;
