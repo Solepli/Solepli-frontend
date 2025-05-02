d33d14120b4ea9a58886890c4b059b328818f715
ion <delion.dev@gmail.com>
2025-05-02 13:28:30 +0900

    [feat/#7] ReviewEmoji 컴포넌트 구현

--- src/components/BottomSheet/ReviewWrite/ReviewWrite.tsx
+++ src/components/BottomSheet/ReviewWrite/ReviewWrite.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
+import ReviewEmoji from './ReviewEmoji';
+
 import xButton from '../../../assets/xButton.svg';
 
 const ReviewWrite: React.FC = () => {
   return (
-    <div className='flex flex-col items-start justify-start pt-0 px-0 pb-[300px] bg-[#ef2d2d] rounded-br-0 rounded-bl-0'>
-      <div className='self-stretch flex flex-row items-center justify-end pt-0 px-[16px] pb-[8px] bg-[#ffdcdc] '>
-        <div className='w-[32px] h-[32px] shrink-0 flex flex-row items-center justify-center bg-[#eceef2] rounded-[8px]'>
+    <div className='flex flex-col items-start justify-start pt-0 px-0 pb-[300px] rounded-br-0 rounded-bl-0'>
+      {/* content title */}
+      <div className='self-stretch flex flex-row items-center justify-end pt-0 px-[16px] pb-[8px]'>
+        <div className='w-[32px] h-[32px] shrink-0 flex flex-row items-center justify-center rounded-[8px]'>
           <img width='24' height='24' src={xButton}></img>
         </div>
       </div>
+
+      {/* 방문 의향 체크 */}
+      <ReviewEmoji />
     </div>
   );
 };
