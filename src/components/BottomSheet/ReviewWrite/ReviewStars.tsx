3d1c926e3c67c82bd65d0d6a7dd41ede85917ce5
ion <delion.dev@gmail.com>
2025-05-02 14:32:07 +0900

    [refactor/#7] ReviewStars에 key 속성 추가

--- src/components/BottomSheet/ReviewWrite/ReviewStars.tsx
+++ src/components/BottomSheet/ReviewWrite/ReviewStars.tsx
@@ -5,8 +5,14 @@ import starEmpty from '../../../assets/starEmpty.svg';
 const ReviewStars: React.FC = () => {
   return (
     <div className='self-stretch flex flex-row items-center justify-center gap-[2px]'>
-      {Array.from({ length: 5 }).map(() => (
-        <img className='p-[4px]' width='32' height='32' src={starEmpty} />
+      {Array.from({ length: 5 }).map((_, i) => (
+        <img
+          className='p-[4px]'
+          width='32'
+          height='32'
+          src={starEmpty}
+          key={i}
+        />
       ))}
     </div>
   );
