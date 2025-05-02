02a95d330046d641e86e93af621493e7ee619fd5
sky121016 <sky121016@naver.com>
2025-05-02 18:51:10 +0900

    [feat/#9] App.js 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Solmap from './pages/Solmap';
-import PreviewContentList from './components/BottomSheet/Preview/PreviewContentList';
-import DetailContent from './components/BottomSheet/DetailContent';
-import CategoryButtonList from './components/BottomSheet/Category/CategoryButtonList';
+
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='solmap' element={<Solmap />}>
-            <Route index element={<CategoryButtonList />} />
-            <Route path='list' element={<PreviewContentList />} />
-            <Route path='detail' element={<DetailContent />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+
     </>
   );
 }
