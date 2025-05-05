434fe958e3b858f3cd5dfec04fbaec5524c49a27
ion <delion.dev@gmail.com>
2025-05-05 17:53:00 +0900

    [refactor/#14] 컴포넌트의 색상을 컬러코드로 변경

--- src/components/Searching/SearchBar.tsx
+++ src/components/Searching/SearchBar.tsx
@@ -6,10 +6,10 @@ const SearchBar: React.FC = () => {
   return (
     <div
       className='shadow-[0_2px_2px_0_rgba(18,18,18,0.1)]
-        h-34 flex-[1_0_0] flex items-center px-8 bg-[#fafafa] rounded-[12px]'>
+        h-34 flex-[1_0_0] flex items-center px-8 bg-white rounded-[12px]'>
       <div className='flex-1 flex items-center justify-start gap-4'>
         <img width='24' height='24' src={search}></img>
-        <div className="flex-1 text-[14px] leading-[100%] tracking-[-0.21px] font-['Pretendard'] text-[#8691aa]">
+        <div className="flex-1 text-[14px] leading-[100%] tracking-[-0.21px] font-['Pretendard'] text-primary-400">
           오늘은 어디서 시간을 보내나요?
         </div>
       </div>
