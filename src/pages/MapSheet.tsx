25d37a5af220dd3c3f358c9e00227fa76e956184
ion <delion.dev@gmail.com>
2025-05-07 06:42:27 +0900

    [feat/#26] 데이터로 마커 찍기

--- src/pages/MapSheet.tsx
+++ src/pages/MapSheet.tsx
@@ -1,18 +1,40 @@
 import React, { useEffect, useRef } from 'react';
+import { places } from '../places';
 
 const MapSheet: React.FC = () => {
   const mapElement = useRef<HTMLDivElement | null>(null);
   const mapInstance = useRef<naver.maps.Map | null>(null);
+  const markers = useRef<naver.maps.Marker[]>([]);
 
   useEffect(() => {
     if (!mapElement.current) return;
 
-    const center = new naver.maps.LatLng(37.3595704, 127.105399);
+    const center = new naver.maps.LatLng(37.513213, 126.94656);
     mapInstance.current = new naver.maps.Map(mapElement.current, {
       center: center,
       zoom: 16,
     });
   }, []);
+
+  useEffect(() => {
+    if (!mapInstance.current) return;
+
+    // 기존 마커 삭제
+    markers.current.forEach((m) => m.setMap(null));
+    markers.current = [];
+
+    markers.current = places.map((place) => {
+      const position = new naver.maps.LatLng(place.latitude, place.longitude);
+
+      const marker = new naver.maps.Marker({
+        position: position,
+        map: mapInstance.current || undefined,
+        title: place.title,
+      });
+
+      return marker;
+    });
+  });
 
   return <div ref={mapElement} className='w-dvw h-dvh' />;
 };
