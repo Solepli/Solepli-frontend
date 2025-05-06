092d0a3b2f26e0e80d402b1139967fd3642ec9e3
sky121016 <sky121016@naver.com>
2025-05-06 23:51:36 +0900

    [feat/#23] 카테고리 선택&필터링 기능 구현

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { usePlaceStore } from '../../store/placeStore';
+import { places } from '../../places';
 
 const BottomSheet: React.FC<{ children: React.ReactNode }> = ({children}) => {
-
+  const setPlaces = usePlaceStore((state)=>state.setPlaces);
+  
+  useEffect(()=>{
+    console.log(places);
+    setPlaces(places);
+  });
 
   return (
     <div className='w-full h-450 bg-white rounded-t-xl flex flex-col z-1 fixed bottom-0 pb-50 shadow-[0_-1px_20px_rgba(0,0,0,0.1)]'>
