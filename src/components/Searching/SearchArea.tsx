cea2ec49e336c54b3a511e80581263bb7611dcd2
ion <delion.dev@gmail.com>
2025-05-06 19:28:36 +0900

    [Refactor/#20] SearchBar, SearchArea 스타일 변경

--- src/components/Searching/SearchArea.tsx
+++ src/components/Searching/SearchArea.tsx
@@ -3,7 +3,9 @@ import SearchBar from './SearchBar';
 
 const SearchArea: React.FC = () => {
   return (
-    <div className='flex flex-row items-center justify-start pt-12 px-16'>
+    <div
+      className='fixed top-0 inset-x-0 z-50
+    flex flex-row items-center justify-start pt-[12px] px-[16px]'>
       <SearchBar />
     </div>
   );
