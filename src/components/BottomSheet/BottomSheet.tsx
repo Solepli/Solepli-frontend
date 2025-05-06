100c53b5b56dc98b22e2538813c794988b8f6566
lth-1026 <th20001026@gmail.com>
2025-05-06 21:40:36 +0900

    [feat/#12] Header를 잡고 bottomsheet를 움직일 수 있음
    transform을 이용해 header를 잡고 bottomsheet를 움직일 수 있게 만들었다.
    top이 최상단일 경우, header를 잡고 내려야지만 bottomsheet를 내릴 수 있다.
    이를 구현하기 위해 Header 컴포넌트에 onPointerDown 함수를 추가했다.

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -5,9 +5,11 @@ import { places } from '../../places';
 import { MAX_Y, MIN_Y, MID_Y, } from '../../constants';
 import { useBottomSheetStore } from '../../store/useBottomSheetStore';
 
-const Header: React.FC = () => {
+const Header: React.FC<{onPointerDown?: React.PointerEventHandler}> = ({onPointerDown}) => {
   return (
-    <div className='w-full h-20 py-8 inline-flex flex-col justify-center items-center'>
+    <div 
+    className='w-full h-20 py-8 inline-flex flex-col justify-center items-center'
+    onPointerDown={onPointerDown}>
       <div className='w-30 h-4 bg-gray-200 rounded-[10px]' />
     </div>
   );
@@ -21,6 +23,7 @@ const BottomSheet: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { snap, setSnap } = useBottomSheetStore();
   const [dragStartY, setDragStartY] = React.useState<number | null>(null);
   const [dragOffset, setDragOffset] = React.useState(0);
+  const isHeader = useRef(false);
 
   const handlePointerDown = (e: React.PointerEvent) => {
     setDragStartY(e.clientY);
@@ -28,6 +31,7 @@ const BottomSheet: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   };
 
   const handlePointerMove = (e: React.PointerEvent) => {
+    if (snap === MAX_Y && !isHeader.current) return; // MAX_Y일 때는 Header를 통해서만 드래그 가능
     if (dragStartY !== null) {
       const offset = dragStartY - e.clientY; //offset이 양수면 위로 드래그, 음수면 아래로 드래그
       setDragOffset(offset);
@@ -48,6 +52,7 @@ const BottomSheet: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       }
       setSnap(nextSnap);
     }
+    isHeader.current = false;
     setDragStartY(null);
     setDragOffset(0);
   };
@@ -57,19 +62,21 @@ const BottomSheet: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   });
 
   const scrollableClass = snap === MAX_Y ? 'overflow-y-auto' : 'overflow-hidden';
+  const isDragging = dragStartY !== null;
 
   return (
     <div
       ref={sheetRef}
-      className='fixed bottom-0 left-0 w-full bg-white rounded-t-2xl z-50 transition-all duration-300 touch-none select-none'
+      className={`fixed bottom-0 left-0 w-full bg-white rounded-t-2xl z-50 ${isDragging ? '' : 'transition-all duration-300'} touch-none select-none pointer-events-auto`}
       onPointerDown={handlePointerDown}
       onPointerMove={handlePointerMove}
       onPointerUp={handlePointerUp}
       style={{
-        height: `${snap}px`,
+        height: `${MAX_Y}px`,
+        transform: `translateY(${MAX_Y - (snap + dragOffset)}px)`,
       }}>
       {/* 드래그 핸들 */}
-      <Header />
+      <Header onPointerDown={() => isHeader.current = true} />
 
       <div className={`h-full ${scrollableClass}`}>
         {/* BottomSheet 내용 */}
