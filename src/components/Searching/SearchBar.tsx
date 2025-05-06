5002351ddd47172dccc1da486c4fcb17497bd6ad
ion <delion.dev@gmail.com>
2025-05-06 21:42:14 +0900

    [feat/#20] SearchBar의 값 입력/삭제 구현

--- src/components/Searching/SearchBar.tsx
+++ src/components/Searching/SearchBar.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
-
 import search from '../../assets/search.svg';
 import useSearchStore from '../../store/searchStore';
+import XButtonCircle from '../XButtonCircle';
 
 const SearchBar: React.FC = () => {
-  const { setIsFocused } = useSearchStore();
+  const { isFocused, setIsFocused, inputValue, setInputValue } =
+    useSearchStore();
 
   return (
     <div
-      className='shadow-[0_2px_2px_0_rgba(18,18,18,0.1)] h-[34px] flex-[1_0_0] flex items-center px-[8px] bg-white rounded-[12px]'
+      className={[
+        isFocused
+          ? 'bg-primary-100'
+          : 'bg-white shadow-[0_2px_2px_0_rgba(18,18,18,0.1)]',
+        'h-[34px] flex-[1_0_0] flex items-center px-[8px] rounded-[12px]',
+      ].join(' ')}
       onClick={() => setIsFocused(true)}>
       <div className='flex-1 flex items-center justify-start gap-[4px]'>
         <img className='w-[24px] h-[24px]' src={search} />
-        <div className='flex items-center gap-2'>
-          <div className="flex-1 text-[14px] leading-[100%] tracking-[-0.21px] font-['Pretendard'] text-primary-400">
-            오늘은 어디서 시간을 보내나요?
-          </div>
+        <div className='flex-[1_0_0] flex items-center gap-2'>
+          <input
+            className="focus:outline-none focus:ring-0 resize-none 
+            flex-[1_0_0] text-[14px] leading-[100%] tracking-[-0.21px] font-['Pretendard'] text-primary-900"
+            type='text'
+            value={inputValue}
+            spellCheck={false}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder='오늘은 어디서 시간을 보내나요?'
+          />
         </div>
       </div>
+      {isFocused && inputValue && (
+        <XButtonCircle onClickFunc={() => setInputValue('')} />
+      )}
     </div>
   );
 };
