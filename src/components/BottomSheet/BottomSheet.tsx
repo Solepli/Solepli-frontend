6de39aebc3d41c04cfee7241c0d9956422d16bc0
lth-1026 <th20001026@gmail.com>
2025-05-07 06:21:50 +0900

    [feat/#12] MapChipList를 BottomSheet header로 이동

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -1,17 +1,28 @@
-// src/components/BottomSheet/BottomSheet.tsx
 import React, { useEffect, useRef } from 'react';
 import { usePlaceStore } from '../../store/placeStore';
 import { places } from '../../places';
-import { MAX_Y, MIN_Y, MID_Y, CATAGORY_MAX_Y, CATAGORY_MIN_Y} from '../../constants';
+import {
+  MAX_Y,
+  MIN_Y,
+  MID_Y,
+  CATAGORY_MAX_Y,
+  CATAGORY_MIN_Y,
+} from '../../constants';
 import { useBottomSheetStore } from '../../store/useBottomSheetStore';
 import { useLocation } from 'react-router-dom';
+import MapChipList from './Category/MapChipList';
 
-const Header: React.FC<{onPointerDown?: React.PointerEventHandler}> = ({onPointerDown}) => {
+const Header: React.FC<{
+  onPointerDown?: React.PointerEventHandler;
+  children?: React.ReactNode;
+  mapChipListRef?: React.RefObject<HTMLDivElement | null>;
+}> = ({ onPointerDown, children }) => {
   return (
-    <div 
-    className='w-full h-20 py-8 inline-flex flex-col justify-center items-center bg-white absolute top-[-1px] z-100'
-    onPointerDown={onPointerDown}>
-      <div className='w-30 h-4 bg-gray-200 rounded-[10px]' />
+    <div onPointerDown={onPointerDown} className='touch-none select-none'>
+      <div className='w-full h-20 py-8 inline-flex flex-col justify-center items-center bg-white z-100'>
+        <div className='w-30 h-4 bg-gray-200 rounded-[10px]' />
+      </div>
+      <div>{children}</div>
     </div>
   );
 };
@@ -23,8 +34,10 @@ const BottomSheet: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [dragStartY, setDragStartY] = React.useState<number | null>(null);
   const [dragOffset, setDragOffset] = React.useState(0);
   const isHeader = useRef(false);
+  const isDragging = useRef(false);
   const location = useLocation();
   const isCategory = location.pathname === '/map';
+  const isList = location.pathname === '/map/list';
 
   // 카테고리일 때는 CATAGORY_MAX_Y로 초기화
   // 카테고리가 아닐 때는 MID_Y로 초기화
@@ -43,9 +56,13 @@ const BottomSheet: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
   const handlePointerMove = (e: React.PointerEvent) => {
     if (snap === MAX && !isHeader.current && !isCategory) return; // MAX_Y일 때는 Header를 통해서만 드래그 가능(카테고리 제외)
+
     if (dragStartY !== null) {
+      isDragging.current = true;
       const offset = dragStartY - e.clientY; //offset이 양수면 위로 드래그, 음수면 아래로 드래그
-      if(offset > 0 && snap === MAX) return; // MAX_Y일 때는 위로 드래그 불가
+      if (Math.abs(offset) < 10) return; // 드래그 감지 최소값
+      if (offset > 0 && snap === MAX) return; // MAX_Y일 때는 위로 드래그 불가
+      if (offset < 0 && snap === MIN) return; // MIN_Y일 때는 아래로 드래그 불가
       setDragOffset(offset);
     }
   };
@@ -56,14 +73,14 @@ const BottomSheet: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     // 위로 드래그
     if (dragOffset > 0 && snap < MAX) {
       if (!MID) return MAX; // MID가 없을 때는 MAX로 고정
-      if (finishY < MID) return MAX;
+      if (finishY <= MID) return MAX;
       if (finishY > MID) return MID;
     }
 
     // 아래로 드래그
     if (dragOffset < 0 && snap > MIN) {
       if (!MID) return MIN; // MID가 없을 때는 MIN으로 고정
-      if (finishY > MID) return MIN;
+      if (finishY >= MID) return MIN;
       if (finishY < MID) return MID;
     }
 
@@ -76,6 +93,7 @@ const BottomSheet: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       setSnap(nextSnap);
     }
     isHeader.current = false;
+    isDragging.current = false;
     setDragStartY(null);
     setDragOffset(0);
   };
@@ -84,13 +102,13 @@ const BottomSheet: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     setPlaces(places);
   });
 
-  const scrollableClass = snap === MAX && !isCategory ? 'overflow-y-auto' : 'overflow-hidden';
-  const isDragging = dragStartY !== null;
+  const scrollableClass =
+    snap === MAX && !isCategory ? 'overflow-y-auto' : 'overflow-hidden';
 
   return (
     <div
       ref={sheetRef}
-      className={`fixed bottom-0 left-0 w-full bg-white rounded-t-2xl shadow-[0px_-1px_20px_0px_rgba(0,0,0,0.10)] z-50 ${isDragging ? '' : 'transition-all duration-300'} touch-none select-none pointer-events-auto`}
+      className={`fixed bottom-0 left-0 w-full bg-white rounded-t-2xl shadow-[0px_-1px_20px_0px_rgba(0,0,0,0.10)] z-50 ${isDragging.current ? '' : 'transition-all duration-300'} touch-none select-none`}
       onPointerDown={handlePointerDown}
       onPointerMove={handlePointerMove}
       onPointerUp={handlePointerUp}
@@ -99,9 +117,11 @@ const BottomSheet: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         transform: `translateY(${MAX - (snap + dragOffset)}px)`,
       }}>
       {/* 드래그 핸들 */}
-      <Header onPointerDown={() => isHeader.current = true} />
+      <Header onPointerDown={() => (isHeader.current = true)}>
+        {isList && <MapChipList />}
+      </Header>
 
-      <div className={`h-full pt-10 pb-50 ${scrollableClass}`}>
+      <div className={`h-full pb-125 ${scrollableClass}`}>
         {/* BottomSheet 내용 */}
         {children}
       </div>
--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
-export const MIN_Y = 70;
-export const MID_Y = window.innerHeight / 2;
+export const BOTTOM_NAV_HEIGHT = 50; // 바텀 네비게이션 높이
+export const MIN_Y = BOTTOM_NAV_HEIGHT + 72;
 export const MAX_Y = window.innerHeight - 70;
-export const CATAGORY_MAX_Y = 300; // 카테고리 최대 높이
-export const CATAGORY_MIN_Y = 100; // 카테고리 최소 높이
+export const MID_Y = window.innerHeight / 2;
+export const CATAGORY_MAX_Y = 320; // 카테고리 최대 높이
+export const CATAGORY_MIN_Y = 120; // 카테고리 최소 높이
