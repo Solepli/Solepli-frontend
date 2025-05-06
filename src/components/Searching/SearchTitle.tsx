410e2e4c4ff54f80be5ac052df53650f9c9aed9b
ion <delion.dev@gmail.com>
2025-05-06 15:46:56 +0900

    [feat/#14] search-title 컴포넌트 구현

---
+++ src/components/Searching/SearchTitle.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface SearchTitleProps {
+  title: string;
+}
+
+const SearchTitle: React.FC<SearchTitleProps> = ({ title }) => {
+  return (
+    <div className='flex pt-24 px-16 pb-12 items-center gap-10 self-stretch border border-solid border-primary-100'>
+      <div className="font-['Pretendard'] text-[14px] font-[600] leading-[120%] tracking-[-0.35px] text-black">
+        {title}
+      </div>
+    </div>
+  );
+};
+
+export default SearchTitle;
