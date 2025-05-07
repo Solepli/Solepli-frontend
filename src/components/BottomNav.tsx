39661519898396c63125a6732cb48d25f6aff2bc
lth-1026 <th20001026@gmail.com>
2025-05-07 13:01:26 +0900

    [feat/#31] 새로운 svg를 이용해 BottomNav 구현

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,28 +1,40 @@
 import { NavLink } from 'react-router-dom';
 import Sollect from '../assets/nav/sollect.svg?react';
+import SollectActive from '../assets/nav/sollect-active.svg?react';
 import Map from '../assets/nav/map.svg?react';
+import MapActive from '../assets/nav/map-active.svg?react';
 import Mark from '../assets/nav/mark.svg?react';
+import MarkActive from '../assets/nav/mark-active.svg?react';
 import Profile from '../assets/nav/profile.svg?react';
+import ProfileActive from '../assets/nav/profile-active.svg?react';
 
 const BottomNav = () => {
   const menu = [
-    { name: 'sollect', path: '/sollect', icon: <Sollect /> },
-    { name: 'map', path: '/map', icon: <Map /> },
-    { name: 'mark', path: '/mark', icon: <Mark /> },
-    { name: 'profile', path: '/profile', icon: <Profile /> },
+    {
+      name: 'sollect',
+      path: '/sollect',
+      icon: <Sollect />,
+      activeIcon: <SollectActive />,
+    },
+    { name: 'map', path: '/map', icon: <Map />, activeIcon: <MapActive /> },
+    { name: 'mark', path: '/mark', icon: <Mark />, activeIcon: <MarkActive /> },
+    {
+      name: 'profile',
+      path: '/profile',
+      icon: <Profile />,
+      activeIcon: <ProfileActive />,
+    },
   ];
 
   return (
     <nav>
-      <div className='w-full shadow-[0px_-4px_20px_rgba(18,_18,_18,_0.08)] px-[30px] pt-[5px] bg-white flex inline-flex justify-between items-center z-150'>
+      <div className='w-full shadow-[0px_-4px_20px_rgba(18,_18,_18,_0.08)] px-30 pt-5 bg-white flex inline-flex justify-between items-center z-150'>
         {menu.map((item) => (
           <NavLink
             key={item.name}
             to={item.path}
-            className={({ isActive }) =>
-              `flex flex-col items-center gap-1 text-[10px] font-pretendard ${isActive ? 'text-gray-900' : 'text-gray-300'}`
-            }>
-            {item.icon}
+            className='flex flex-col items-center gap-1'>
+            {({ isActive }) => (isActive ? item.activeIcon : item.icon)}
           </NavLink>
         ))}
       </div>
