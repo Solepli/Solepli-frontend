a4f9a4f380138ad5505de37942692cd38d4c7c81
sky121016 <sky121016@naver.com>
2025-05-12 00:04:00 +0900

    [refactor/#40] places.ts의 places 데이터에 tags와 hours 추가

--- src/places.ts
+++ src/places.ts
@@ -1,43 +1,88 @@
 export const places = [
-    {
-      title: '스타벅스 노량진동점',
-      address:
-        '서울 동작구 노량진로 190',
-      latitude: 37.513213,
-      longitude: 126.94656,
-      category: {title:"카페", id:"cafe"},
-      rating: 4.5,
-      tags:["조용한","편안한","낮에 가기 좋은"],
-    },
-  
-    {
-      title: '도로도로커피숍',
-      address: '서울 동작구 노량진로 145 1층',
-      latitude: 37.51388,
-      longitude: 126.942058,
-      category: {title:"카페", id:"cafe"},
-      rating: 4.2,
-      tags:["조용한","편안한","오래 머물기 좋은"],
-    },
-    {
-      title: '미분당',
-      address:
-        '서울 동작구 노량진로 190',
-      latitude: 37.5132,
-      longitude: 126.946,
-      category: {title:"식당", id:"food"},
-      rating: 4.5,
-      tags:["조용한","편안한","낮에 가기 좋은"],
-    },
-  
-    {
-      title: '샤브로',
-      address: '서울 동작구 노량진로 145 1층',
-      latitude: 37.513,
-      longitude: 126.9420,
-      category: {title:"식당", id:"food"},
-      rating: 4.2,
-      tags:["트렌디한","편안한","1인 메뉴가 있는"],
-    },
-  ];
-  
+  {
+    title: '스타벅스 노량진동점',
+    address: '서울 동작구 노량진로 190',
+    latitude: 37.513213,
+    longitude: 126.94656,
+    category: { title: '카페', id: 'cafe' },
+    rating: 4.5,
+    tags: [
+      { id: 'quiet', text: '조용한' },
+      { id: 'lively', text: '시끌벅적한' },
+      { id: 'cozy', text: '편안한' },
+    ],
+    hours: [
+      { day: 0, startTime: 8, endTime: 16 },
+      { day: 1, startTime: 8, endTime: 16 },
+      { day: 2, startTime: 8, endTime: 16 },
+      { day: 3, startTime: 8, endTime: 16 },
+      { day: 4, startTime: 8, endTime: 16 },
+      { day: 5, startTime: 8, endTime: 16 },
+    ],
+  },
+
+  {
+    title: '도로도로커피숍',
+    address: '서울 동작구 노량진로 145 1층',
+    latitude: 37.51388,
+    longitude: 126.942058,
+    category: { title: '카페', id: 'cafe' },
+    rating: 4.2,
+    tags: [
+      { id: 'quiet', text: '조용한' },
+      { id: 'lively', text: '시끌벅적한' },
+      { id: 'cozy', text: '편안한' },
+    ],
+    hours: [
+      { day: 0, startTime: 8, endTime: 16 },
+      { day: 1, startTime: 8, endTime: 16 },
+      { day: 2, startTime: 8, endTime: 16 },
+      { day: 3, startTime: 8, endTime: 16 },
+      { day: 4, startTime: 8, endTime: 16 },
+      { day: 5, startTime: 8, endTime: 16 },
+    ],
+  },
+  {
+    title: '미분당',
+    address: '서울 동작구 노량진로 190',
+    latitude: 37.5132,
+    longitude: 126.946,
+    category: { title: '식당', id: 'food' },
+    rating: 4.5,
+    tags: [
+      { id: 'luxurious', text: '고급스러운' },
+      { id: 'unique', text: '색다른' },
+      { id: 'hip', text: '힙한' },
+    ],
+    hours: [
+      { day: 0, startTime: 8, endTime: 16 },
+      { day: 1, startTime: 8, endTime: 16 },
+      { day: 2, startTime: 8, endTime: 16 },
+      { day: 3, startTime: 8, endTime: 16 },
+      { day: 4, startTime: 8, endTime: 16 },
+      { day: 5, startTime: 8, endTime: 16 },
+    ],
+  },
+
+  {
+    title: '샤브로',
+    address: '서울 동작구 노량진로 145 1층',
+    latitude: 37.513,
+    longitude: 126.942,
+    category: { title: '식당', id: 'food' },
+    rating: 4.2,
+    tags: [
+      { id: 'luxurious', text: '고급스러운' },
+      { id: 'unique', text: '색다른' },
+      { id: 'hip', text: '힙한' },
+    ],
+    hours: [
+      { day: 0, startTime: 8, endTime: 16 },
+      { day: 1, startTime: 8, endTime: 16 },
+      { day: 2, startTime: 8, endTime: 16 },
+      { day: 3, startTime: 8, endTime: 16 },
+      { day: 4, startTime: 8, endTime: 16 },
+      { day: 5, startTime: 8, endTime: 16 },
+    ],
+  },
+];
