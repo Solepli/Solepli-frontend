dc80aee9fca6b05be19723568d3702fba0ecbeb9
sky121016 <sky121016@naver.com>
2025-05-17 16:24:43 +0900

    [refactor/#50] DetailContent parameter의 placeId로 place 상세 정보 받아오도록 수정

--- src/components/BottomSheet/DetailContent.tsx
+++ src/components/BottomSheet/DetailContent.tsx
@@ -1,51 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ContentTitle from './ContentTitle';
-import { places } from '../../places';
 import ReviewRange from './ReviewRange';
 import TagList from './TagList';
 import ReviewPhotos from './ReviewPhotos';
 import ReviewList from './Review/ReviewList';
+import { fetchPlaceById } from '../../api/placeApi';
+import { usePlaceStore } from '../../store/placeStore';
+import { useParams } from 'react-router-dom';
 
 const DetailContent: React.FC = () => {
-  const place = places[0];
-  const tags1 = [
-      { id: 'quiet', text: '조용한' },
-      { id: 'lively', text: '시끌벅적한' },
-      { id: 'cozy', text: '편안한' },
-    ];
-  const tags2 = [
-      { id: 'luxurious', text: '고급스러운' },
-      { id: 'unique', text: '색다른' },
-      { id: 'hip', text: '힙한' },
-    ];
+  const { selectedPlace } = usePlaceStore();
   const counts = [2, 3, 1];
+
+  const { setPlace } = usePlaceStore();
 
   const images = [
     'https://images.ctfassets.net/rric2f17v78a/1ql70crfzaiw9nFd58CZ7p/04652a19ab2fe5a5370d92c7957eb016/open-a-bakery-header.jpg',
     'https://i.namu.wiki/i/PgSYmu9y55E5YicKvIK14P0ttQUQG4ioSn-Fd6u27a0r2Jeu02fJAYRkmf2qtOb6fHLBnlrLeXu_gSESQbmykg.webp',
     'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/cc/5b/8f/various-breads.jpg?w=800&h=-1&s=1',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvcP45F8yq6R7WMSjpuU0JAOh0foZEOSPr9g&s'
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvcP45F8yq6R7WMSjpuU0JAOh0foZEOSPr9g&s',
   ];
+
+  const { placeId } = useParams<{ placeId: string }>();
+
+  useEffect(() => {
+    if (placeId) {
+      fetchPlaceById(parseInt(placeId)).then((fetchedPlace) => {
+        setPlace(fetchedPlace);
+      });
+    }
+  }, [placeId]);
+
+  if (!selectedPlace) {
+    return null;
+  }
 
   return (
     <div>
       {/* ContentTitle */}
-      <ContentTitle place={place} property='detail' />
+      <ContentTitle place={selectedPlace} property='detail' />
 
       {/* ReviewRange */}
-      <ReviewRange rating={place.rating} recommend={90} />
+      <ReviewRange rating={selectedPlace.rating} recommend={90} />
 
       {/* tags */}
       <div className='pb-12'>
-        <TagList headerName='분위기' tags={tags1} counts={counts} />
-        <TagList headerName='1인 이용' tags={tags2} counts={counts} />
+        <TagList headerName='분위기' tags={selectedPlace.tags} counts={counts} />
+        <TagList headerName='1인 이용' tags={selectedPlace.tags} counts={counts} />
       </div>
 
       {/* ReviewPhotoList */}
       <ReviewPhotos images={images} />
 
       {/* ReviewList */}
-      <ReviewList placeId={place.id} />
+      <ReviewList placeId={selectedPlace.id} />
     </div>
   );
 };
