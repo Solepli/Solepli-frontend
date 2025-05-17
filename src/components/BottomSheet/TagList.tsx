a8e4cb239dcbc35bfd8587bc8f70bd9bf63677f0
sky121016 <sky121016@naver.com>
2025-05-17 14:19:54 +0900

    [refactor/#50] 태그 가로 스크롤

--- src/components/BottomSheet/TagList.tsx
+++ src/components/BottomSheet/TagList.tsx
@@ -7,10 +7,10 @@ interface TagListProps{
     headerName?: string;
     counts?:number[];
 }
-// Tag type과 Tag 컴포넌트 이름이 겹쳐서 문제가 있음
+
 const TagList:React.FC<TagListProps> = ({tags, headerName, counts}) => {
   return (
-    <div className='flex gap-4 px-16 pt-2 pb-4 whitespace-nowrap overflow-x-scroll overflow-y-hidden touch-pan-x'>
+    <div className='flex gap-8 px-16 pt-2 pb-4 whitespace-nowrap overflow-x-scroll overflow-y-hidden touch-pan-x'>
       {headerName && <Tag name={headerName} header/>}
         {tags.map((tag, i) => {
           return <Tag name={tag.text} number={counts && counts[i]} />;
