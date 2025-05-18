74d181f9c6484d6eaace0256967b7e9d1fb62c1b
ion <delion.dev@gmail.com>
2025-05-18 21:55:05 +0900

    [feat/#53] RecentSearch 컴포넌트에 XButtonCircle 컴포넌트 사용

--- src/components/Searching/RecentSearch.tsx
+++ src/components/Searching/RecentSearch.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import xBottonCircle from '../../assets/xButtonCircle.svg';
+import XButtonCircle from '../XButtonCircle';
 
 interface RecentSearchTextProps {
   text: string;
 }
 
 const RecentSearch: React.FC<RecentSearchTextProps> = ({ text }) => {
+  function onClickDeleteRow(): void {
+    console.log('Click XButtonCircle');
+  }
+
   return (
     <div className='self-stretch flex items-center p-[12px_16px_0px_16px]'>
       <div className='flex items-center gap-4 flex-[1_0_0]'>
         <div className="flex-[1_0_0] text-[12px] leading-[120%] tracking-[-0.18px] font-['Pretendard'] text-black">
           {text}
         </div>
-        <img className='w-24 h-24' src={xBottonCircle} alt='xBottonCircle' />
+        <XButtonCircle onClickFunc={() => onClickDeleteRow()} />
       </div>
     </div>
   );
