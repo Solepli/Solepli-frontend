ba6d1fe0bc125bed9d3ed92242226c770de6062b
sky121016 <sky121016@naver.com>
2025-05-18 22:47:10 +0900

    [feat/#56] 사진 더보기 기능 구현

--- src/components/BottomSheet/ReviewPhotos.tsx
+++ src/components/BottomSheet/ReviewPhotos.tsx
@@ -1,21 +1,42 @@
 import React from 'react';
+import arrowTail from '../../assets/arrowTail.svg';
+import { useNavigate, useParams } from 'react-router-dom';
 
 interface ReviewPhotosProps {
   images: string[];
+  more?: boolean;
 }
 
-const ReviewPhotos: React.FC<ReviewPhotosProps> = ({ images }) => {
+const ReviewPhotos: React.FC<ReviewPhotosProps> = ({ images, more }) => {
+  const navigate = useNavigate();
+  const {placeId} = useParams();
+
+  const handleClick = ()=>{
+    navigate(`/map/reviews/${placeId}`);
+  }
 
   return (
     <div className='px-16 pb-20 gap-2 flex overflow-x-scroll touch-pan-x'>
       {images.map((image, i) => {
+        const isLast = i === images.length - 1;
+
         return (
-          <img
-            src={image}
-            alt=''
-            className={`w-100 h-125 object-cover rounded-sm ${i === 0 ? 'rounded-tl-[20px]' : ''} ${i === images.length-1 ? 'rounded-br-[20px]' : ''}`}
-            key={i}
-          />
+          <div key={i} className='shrink-0 relative'>
+            <img
+              src={image}
+              alt='review-image'
+              className={`w-100 h-125 object-cover rounded-sm ${isLast && more ? 'brightness-20' : ''} ${i === 0 ? 'rounded-tl-[20px]' : ''} ${i === images.length - 1 ? 'rounded-br-[20px]' : ''}`}
+            />
+
+            {isLast && more && (
+              <button className='absolute top-40 left-34 text-xs text-white flex flex-col justify-center items-center gap-4' onClick={handleClick}>
+                <div className='bg-white rounded-full w-24 h-24 flex justify-center items-center'>
+                  <img src={arrowTail} alt='arrow' className='w-24 h-24' />
+                </div>
+                <p>더보기</p>
+              </button>
+            )}
+          </div>
         );
       })}
     </div>
