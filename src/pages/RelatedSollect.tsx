8463cd795a9c990e6a1ba02c34d1385e688d7ae9
sky121016 <sky121016@naver.com>
2025-05-19 20:15:51 +0900

    [feat/#63] SollectPhoto 컴포넌트 생성

--- src/pages/RelatedSollect.tsx
+++ src/pages/RelatedSollect.tsx
@@ -1,17 +1,79 @@
-import React from 'react'
+import React from 'react';
 import TitleHeader from '../components/global/TitleHeader';
 import { useNavigate } from 'react-router-dom';
-
+import SollectPhoto from '../components/Sollect/SollectPhoto';
 
 const RelatedSollect = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const sollectData = [
+    {
+      id: 1,
+      title: '성수동 혼놀 코스',
+      address: '성동구, 성수동2가',
+      imageUrl:
+        'https://cdn.imweb.me/upload/S202207276eb21328c800f/0d1de28381aef.jpg',
+    },
+    {
+      id: 2,
+      title: '잠시 쉬어가는 시간',
+      address: '성동구, 성수동2가',
+      imageUrl:
+        'https://cdn.imweb.me/upload/S202207276eb21328c800f/0d1de28381aef.jpg',
+    },
+    {
+      id: 3,
+      title: '성수동 혼놀 코스',
+      address: '성동구, 성수동2가',
+      imageUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH8HvHrjwd0T3122vCeMXg79UAWqixsmLBLVEoQol3KlaZEsY4_LrR1sLtcWYqGdjIciA&usqp=CAU',
+    },
+    {
+      id: 4,
+      title: '성수동 혼놀 코스',
+      address: '성동구, 성수동2가',
+      imageUrl:
+        'https://cdn.imweb.me/upload/S202207276eb21328c800f/0d1de28381aef.jpg',
+    },
+    {
+      id: 5,
+      title: '성수동 혼놀 코스',
+      address: '성동구, 성수동2가',
+      imageUrl:
+        'https://cdn.imweb.me/upload/S202207276eb21328c800f/0d1de28381aef.jpg',
+    },
+    {
+      id: 6,
+      title: '성수동 혼놀 코스',
+      address: '성동구, 성수동2가',
+      imageUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH8HvHrjwd0T3122vCeMXg79UAWqixsmLBLVEoQol3KlaZEsY4_LrR1sLtcWYqGdjIciA&usqp=CAU',
+    },
+    {
+      id: 7,
+      title: '성수동 혼놀 코스',
+      address: '성동구, 성수동2가',
+      imageUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH8HvHrjwd0T3122vCeMXg79UAWqixsmLBLVEoQol3KlaZEsY4_LrR1sLtcWYqGdjIciA&usqp=CAU',
+    },
+    {
+      id: 8,
+      title: '성수동 혼놀 코스',
+      address: '성동구, 성수동2가',
+      imageUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH8HvHrjwd0T3122vCeMXg79UAWqixsmLBLVEoQol3KlaZEsY4_LrR1sLtcWYqGdjIciA&usqp=CAU',
+    },
+  ];
   return (
     <div>
-            <TitleHeader title="관련 쏠렉트" onClick={()=>navigate(-1)}/>
-
-
+      <TitleHeader title='관련 쏠렉트' onClick={() => navigate(-1)} />
+    
+      <div className='flex flex-wrap gap-12 justify-center pt-58'>
+        {sollectData.map((sollect) => {
+          return <SollectPhoto sollect={sollect} />;
+        })}
+      </div>
     </div>
   );
-}
+};
 
-export default RelatedSollect
+export default RelatedSollect;
