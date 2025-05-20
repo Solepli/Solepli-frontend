351a109fadff209a0da37fe1ad1621e5afe5df41
sky121016 <sky121016@naver.com>
2025-05-20 23:54:35 +0900

    [feat/#70] 쏠렉트 페이지 구현

---
+++ src/pages/SollectPage.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import SollectGNB from '../components/Sollect/SollectGNB'
+import PopularSollectList from '../components/Sollect/PopularSollectList';
+
+const SollectPage = () => {
+  return (
+    <div>
+      {/* GNB */}
+      <SollectGNB />
+
+      {/* popular Sollect */}
+      <h1 className='text-primary-950 text-xl font-bold pt-12 px-20 pb-8'>인기 많은 솔렉트</h1>
+      <PopularSollectList />
+
+
+      {/* Sollect Chip List */}
+
+      {/* Sollect Group */}
+    </div>
+  );
+}
+
+export default SollectPage
