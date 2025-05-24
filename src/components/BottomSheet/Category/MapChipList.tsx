961f3393d7d22e31ad766fb877600d0be04eca09
sky121016 <sky121016@naver.com>
2025-05-24 15:28:24 +0900

    [feat/#70] util에 icon 추가

--- src/components/BottomSheet/Category/MapChipList.tsx
+++ src/components/BottomSheet/Category/MapChipList.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import { Category } from '../../../types';
 import MapChip from './MapChip';
+import { categories } from '../../../utils/category';
 
 const MapChipList: React.FC = () => {
-    const categories: Category[] = [
-      { title: '식당', id: 'food' },
-      { title: '카페', id: 'cafe' },
-      { title: '주점', id: 'drink' },
-      { title: '쇼핑', id: 'shop' },
-      { title: '공부/작업', id: 'work' },
-      { title: '산책', id: 'walk' },
-      { title: '문화/예술', id: 'culture' },
-      { title: '오락/여가', id: 'entertainment' },
-    ];
+  
     return (
       <div className='flex gap-10 p-16 pt-8 h-54 whitespace-nowrap overflow-x-scroll overflow-y-hidden border-b border-primary-100 shrink-0 sticky top-0 bg-white z-1 touch-pan-x'>
         {categories.map((category) => {
