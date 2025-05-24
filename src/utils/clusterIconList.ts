6d76a4a67569ae4386697a659878da03611587c1
ion <delion.dev@gmail.com>
2025-05-24 15:18:16 +0900

    [feat/#77] 클러스터 아이콘을 설정하기 위한 clusterIconList 작성
    클러스터 마다 내부 마커 리스트를 기반으로, 최상단 마커의 카테고리에 맞는
    아이콘을 반환하는 clusterIconList를 작성함.

---
+++ src/utils/clusterIconList.ts
@@ -0,0 +1,42 @@
+export const clusterIconList = {
+  food: {
+    url: '/src/assets/cluster/foodCluster.svg',
+    size: new naver.maps.Size(40, 40),
+    anchor: new naver.maps.Point(20, 20),
+  },
+  cafe: {
+    url: '/src/assets/cluster/cafeCluster.svg',
+    size: new naver.maps.Size(40, 40),
+    anchor: new naver.maps.Point(20, 20),
+  },
+  drink: {
+    url: '/src/assets/cluster/drinkCluster.svg',
+    size: new naver.maps.Size(40, 40),
+    anchor: new naver.maps.Point(20, 20),
+  },
+  shop: {
+    url: '/src/assets/cluster/shopCluster.svg',
+    size: new naver.maps.Size(40, 40),
+    anchor: new naver.maps.Point(20, 20),
+  },
+  work: {
+    url: '/src/assets/cluster/workCluster.svg',
+    size: new naver.maps.Size(40, 40),
+    anchor: new naver.maps.Point(20, 20),
+  },
+  walk: {
+    url: '/src/assets/cluster/walkCluster.svg',
+    size: new naver.maps.Size(40, 40),
+    anchor: new naver.maps.Point(20, 20),
+  },
+  culture: {
+    url: '/src/assets/cluster/cultureCluster.svg',
+    size: new naver.maps.Size(40, 40),
+    anchor: new naver.maps.Point(20, 20),
+  },
+  entertainment: {
+    url: '/src/assets/cluster/entertainmentCluster.svg',
+    size: new naver.maps.Size(40, 40),
+    anchor: new naver.maps.Point(20, 20),
+  },
+};
