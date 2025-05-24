352fad22d8f9b72152253e97eab21c62c2b707b9
ion <delion.dev@gmail.com>
2025-05-24 15:38:36 +0900

    [refactor/#77] 클러스터 생성 시 기본 아이콘 적용 및 defaultCluster 아이콘 추가
    클러스터 객체를 생성할 때 기본 아이콘이 적용되도록 clusterManager.ts 파일을 수정함.
    카테고리별 최상단 마커 종류에 따른 클러스터 아이콘이 적용이 어려울 수도 있어서, defaultCluster 아이콘을 추가하여,
    모든 클러스터에 아이콘이 누락되지 않도록 처리함.

--- src/utils/clusterManager.ts
+++ src/utils/clusterManager.ts
@@ -4,12 +4,6 @@ export const initCluster = (
   markerArray: naver.maps.Marker[],
   map: naver.maps.Map
 ) => {
-  const htmlMarker1 = {
-    url: './food_Cluster.png',
-    size: new naver.maps.Size(40, 40),
-    anchor: new naver.maps.Point(20, 20),
-  };
-
   const markerClustering = new MarkerClustering({
     map,
     markers: markerArray,
@@ -17,17 +11,12 @@ export const initCluster = (
     minClusterSize: 2,
     maxZoom: 20,
     gridSize: 120,
-    icons: [htmlMarker1],
-    indexGenerator: [10],
+    icons: [
+      {
+        url: '/src/assets/cluster/defaultCluster.svg',
+      },
+    ],
     averageCenter: true,
-    stylingFunction: (clusterMarker: any, count: number) => {
-      const firstChild = clusterMarker
-        .getElement()
-        .querySelector('div:first-child');
-      if (firstChild) {
-        firstChild.innerHTML = count;
-      }
-    },
   });
 
   return markerClustering;
