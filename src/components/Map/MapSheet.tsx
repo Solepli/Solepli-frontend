6732ae4455671532f565834dfcd1a2601ad49a6a
ion <delion.dev@gmail.com>
2025-05-28 00:32:42 +0900

    [refactor/#96] 마커 관련 함수 addMarkers, deleteMarkers를 markerFunc.ts로 분리
    지도에 마커를 추가하고 제거하는 addMarkers, deleteMarkers 함수를
    markerFunc.ts로 분리함.
    
    MapSheet에서 로직을 분리하여 코드 가독성과 유지보수성을 향상시킴.

--- src/components/Map/MapSheet.tsx
+++ src/components/Map/MapSheet.tsx
@@ -5,11 +5,10 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchPlacesNearby } from '../../api/mapApi';
 import { useMapStore } from '../../store/mapStore';
 import { useShallow } from 'zustand/shallow';
-import { MarkersInfoType } from '../../types';
 import { useMarkersStore } from '../../store/markersStore';
 import { initCluster } from '../../utils/clusterManager';
-import { IconMarkerMap } from '../../utils/icon';
 import { getCurrentBounds, getUserLatLng, initMap } from '../../utils/mapFunc';
+import { addMarkers, deleteMarkers } from '../../utils/markerFunc';
 
 const MapSheet: React.FC = () => {
   const mapElement = useRef<HTMLDivElement | null>(null);
@@ -72,58 +71,10 @@ const MapSheet: React.FC = () => {
   useEffect(() => {
     console.log('mapMarkers :::', markersInfo);
     if (markersInfo.length > 0) {
-      deleteMarkers();
-      addMarkers();
+      deleteMarkers(markersObject, clearMarkersObject);
+      addMarkers(mapInstance, markersInfo, setMarkersObject);
     }
   }, [markersInfo]);
-
-  const addMarkers = () => {
-    if (!mapInstance.current) return;
-
-    const bounds = new naver.maps.LatLngBounds(
-      new naver.maps.LatLng(0, 0),
-      new naver.maps.LatLng(0, 0)
-    );
-    boundsRef.current = bounds;
-
-    markersInfo.map((place: MarkersInfoType) => {
-      const position = new naver.maps.LatLng(place.latitude, place.longitude);
-      bounds.extend(position);
-
-      const marker = new naver.maps.Marker({
-        position: position,
-        icon: {
-          url: IconMarkerMap[place.category],
-        },
-      });
-      marker.setMap(mapInstance.current);
-      setMarkersObject(marker);
-
-      // 마커 클릭시 지정한 좌표와 줌 레벨을 사용하는 새로운 위치로 지도를 이동
-      // 유사한 함수 : setCenter, panTo
-      naver.maps.Event.addListener(marker, 'click', () => {
-        mapInstance.current?.morph(position, 18, {
-          duration: 1000,
-          easing: 'easeOutCubic',
-        });
-      });
-    });
-    // const clustering = initCluster(markedMarkers, mapInstance.current);
-    /*
-     * todo : naver cloud api map forum에서 클러스터별 최상단 마커의 종류에 따른 (클러스터 아이콘) 설정이 가능하다고 답변받을시
-     * clustering.setIcons([clusterIconList[카테고리]])를 사용하여 클러스터 아이콘 지정 구현
-     */
-  };
-
-  const deleteMarkers = () => {
-    if (markersObject.length === 0) return;
-
-    boundsRef.current = null;
-    markersObject.forEach((m) => {
-      m.setMap(null);
-    });
-    clearMarkersObject();
-  };
 
   // 임시 버튼: 표시된 마커 기준으로 지도 이동
   // const moveToMarkers = useCallback(() => {
