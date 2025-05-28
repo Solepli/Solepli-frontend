2c98678df8281ffff8cd1cc6af7cdc6fd9164290
ion <delion.dev@gmail.com>
2025-05-28 10:40:44 +0900

    [feat/#85] '이 지역에서 검색' 버튼 UI 구현 및 기능 연결
    사용자가 현재 보고 있는 지도 영역에서 장소를 검색할 수 있도록
    '이 지역에서 검색' 버튼을 구현하고, 클릭 시 getCurrentBounds를 호출하여
    지도 중심 좌표를 기반으로 fetchPlacesNearby API를 실행하도록 기능 연결함.

---
+++ src/components/BottomSheet/ReloadMarkerButton.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Compass from '../../assets/compass.svg?react';
+import { ReloadMarkerButtonProps } from '../../interface';
+import { MAX_Y } from '../../constants';
+import { useBottomSheetStore } from '../../store/bottomSheetStore';
+
+const ReloadMarkerButton: React.FC<ReloadMarkerButtonProps> = ({
+  handleClick,
+}) => {
+  const { snap } = useBottomSheetStore();
+  const isVisible = snap !== MAX_Y;
+
+  return (
+    <div
+      style={{
+        width: '100%',
+        position: 'absolute',
+        bottom: `${(snap || 0) + 6}px`,
+        display: isVisible ? 'flex' : 'none',
+        alignItems: 'center',
+        justifyContent: 'center',
+        transition: 'bottom 0.2s ease-in-out',
+      }}>
+      <div
+        className='inline-flex h-32 py-16 pr-12 pl-8 items-center shrink-0 rounded-[100px] bg-primary-50 shadow-[0_1_2_0_rgba(18,18,18,0.1)] '
+        onClick={handleClick}>
+        <div className='flex w-24 h-24 p-4 justify-center items-center'>
+          <Compass />
+        </div>
+        <p className='text-primary-700 text-xs font-medium leading-[120%] tracking-[-0.18px]'>
+          이 지역에서 검색
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ReloadMarkerButton;
