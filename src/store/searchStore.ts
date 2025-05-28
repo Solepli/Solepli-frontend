b8b7f78df04c7d2d4bfb5ee23930b5ca5d5ba13b
ion <delion.dev@gmail.com>
2025-05-28 20:41:32 +0900

    [feat/#100] 연관 검색어 결과를 저장할 zustand 변수 추가 및 타입 수정
    SearchBar.tsx에서 연관 검색어 API 호출 결과를 상태로 관리하기 위해
    zustand에 관련 변수를 추가하고, 응답 데이터 형식에 맞게 타입을 정의 및 수정함.

--- src/store/searchStore.ts
+++ src/store/searchStore.ts
@@ -1,15 +1,25 @@
 import { create } from 'zustand';
+import { RelatedSearchWord } from '../types';
 
 interface SearchState {
   isFocused: boolean;
   setIsFocused: (focused: boolean) => void;
+
   inputValue: string;
   setInputValue: (value: string) => void;
+
+  relatedSearchList: RelatedSearchWord[];
+  setRelatedSearchList: (relatedSearchList: RelatedSearchWord[]) => void;
 }
 
 export const useSearchStore = create<SearchState>((set) => ({
   isFocused: false,
   setIsFocused: (focused) => set({ isFocused: focused }),
+
   inputValue: '',
   setInputValue: (value) => set({ inputValue: value }),
+
+  relatedSearchList: [],
+  setRelatedSearchList: (relatedSearchList) =>
+    set({ relatedSearchList: relatedSearchList }),
 }));
