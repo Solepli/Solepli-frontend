284834e552637495f60578330b0b996a3a2e107a
ion <delion.dev@gmail.com>
2025-06-01 11:14:43 +0900

    [refactor/#110] 함수명 및 타입명 변경

--- src/api/mapApi.ts
+++ src/api/mapApi.ts
@@ -1,7 +1,7 @@
 import { publicAxios } from './axios';
 import { ENDPOINT } from './urls';
 
-export const fetchPlacesNearby = async (
+export const getDisplayMarkers = async (
   swX: number,
   swY: number,
   neX: number,
@@ -21,28 +21,4 @@ export const fetchPlacesNearby = async (
   console.log('response.data.data :::', response.data.data);
 
   return response.data.data;
-};
-
-export const fetchMockPlacesNearby = async () => {
-  await new Promise((res) => setTimeout(res, 500));
-
-  return mockPlacesNearby;
-};
-
-const mockPlacesNearby = {
-  places: [
-    {
-      id: 123,
-      latitude: 37.51123,
-      longitude: 127.05845,
-      category: '산책',
-    },
-    {
-      id: 456,
-      latitude: 37.51345,
-      longitude: 127.06234,
-      category: '카페',
-    },
-  ],
-  categories: ['카페', '디저트', '공원', '산책'],
 };
