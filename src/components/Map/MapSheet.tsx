92f0042b165f1c6c9f32810a93e4be182f8aa078
ion <delion.dev@gmail.com>
2025-06-03 20:49:48 +0900

    [fix/#124] MapSheet 파일 수정

--- src/components/Map/MapSheet.tsx
+++ src/components/Map/MapSheet.tsx
@@ -2,74 +2,68 @@
 import React, { useCallback, useEffect, useLayoutEffect, useRef } from 'react';
 import CurrentLocationButton from '../BottomSheet/CurrentLocationButton';
 import ReloadMarkerButton from '../BottomSheet/ReloadMarkerButton';
-import { useQuery } from '@tanstack/react-query';
 import { getDisplayMarkers } from '../../api/mapApi';
 import { useMapStore } from '../../store/mapStore';
 import { useShallow } from 'zustand/shallow';
-import { useMarkersStore } from '../../store/markersStore';
-import { MarkersInfoType } from '../../types';
-import { IconMarkerMap } from '../../utils/icon';
+import { useMarkerStore } from '../../store/markerStore';
+import { getUserLocation } from '../../utils/geolocation';
 
-// 유저의 현재 위치를 반환하는 함수 (지금은 사용자의 현재 좌표를 서울 지역으로 하드코딩)
-const getUserLatLng = async (): Promise<naver.maps.LatLng> => {
-  try {
-    // const position = await new Promise<GeolocationPosition>((res, rej) => {
-    //   navigator.geolocation.getCurrentPosition(res, rej);
-    // });
-    // return new naver.maps.LatLng(position.coords.latitude, position.coords.latitude);
-    return new naver.maps.LatLng(37.51234, 127.060395);
-  } catch {
-    return new naver.maps.LatLng(37.5666805, 126.9784147); // 기본 좌표 (서울 시청)
-  }
-};
+import {
+  createMarkerObjectList, // 마커를 객체로 생성 후 반환
+} from '../../utils/mapFunc';
 
-// 화면에 보여지는 지도 영역 좌표를 반환하는 함수
-const getDisplayBounds = (
-  InstanceCurrent: naver.maps.Map | null
-): naver.maps.Bounds => {
-  const bounds: naver.maps.Bounds = InstanceCurrent!.getBounds();
-  return bounds;
-};
-
-// divRef에 지도를 생성해서 만들어진 지도 객체를 mapRef에 담는 함수
+/* 지도 생성 */
 const initMap = (
   divRef: React.RefObject<HTMLDivElement | null>,
   mapRef: React.RefObject<naver.maps.Map | null>,
-  center: naver.maps.LatLng
+  center: naver.maps.LatLng,
+  isSearchBounds: boolean,
+  lastBounds: naver.maps.Bounds | undefined,
+  lastZoom: number
 ) => {
-  if (!divRef.current) {
-    console.warn('ElementCurrent(mapElement.current)가 null입니다!');
-    return;
-  }
+  if (!divRef.current) return;
 
   const MapOptions = {
-    center,
-    zoom: 13,
+    zoom: lastZoom || 16,
+    center: lastBounds?.getCenter() || center,
     gl: true,
     customStyleId: import.meta.env.VITE_MAP_STYLE_ID,
+    scaleControl: false,
+    mapDataControl: false,
+    logoControl: false,
+    keyboardShortcuts: false,
+    disableKineticPan: false,
+  } as naver.maps.MapOptions & {
+    bounds?: naver.maps.Bounds;
+    padding?: naver.maps.padding;
   };
+
+  // 조건부로 bounds, padding 추가 (center, zoom 무시)
+  if (isSearchBounds && lastBounds) {
+    MapOptions.bounds = lastBounds;
+    MapOptions.padding = { top: 75, right: 50, bottom: 175, left: 50 };
+  }
+
+  // divRef에 지도를 생성
   const map = new naver.maps.Map(divRef.current!, MapOptions);
+  // mapRef에 객체 지정
   mapRef.current = map;
+
   return map;
 };
 
-// 지도에 인자로 받은 마커 객체 리스트를 표시하는 함수
+/* 마커 추가 함수 */
 const addMarkers = (
   mapRef: React.RefObject<naver.maps.Map | null>,
-  markerObjectList: naver.maps.Marker[]
+  objectList: naver.maps.Marker[] | null
 ) => {
-  if (!mapRef.current) return;
-  // const bounds = new naver.maps.LatLngBounds(
-  //   new naver.maps.LatLng(0, 0),
-  //   new naver.maps.LatLng(0, 0)
-  // );
-  // boundsRef.current = bounds;
+  if (!mapRef.current || !objectList) return;
 
-  markerObjectList.map((m: naver.maps.Marker) => {
-    // 마커 객체를 지도 객체에 설정
+  objectList.forEach((m: naver.maps.Marker) => {
+    // 지도에 마커 객체 설정
     m.setMap(mapRef.current);
 
-    // 마커 클릭시 지정한 좌표와 줌 레벨을 사용하는 새로운 위치로 지도를 이동 (유사한 함수 : setCenter, panTo)
+    // 이벤트 리스너 (마커 클릭)
     naver.maps.Event.addListener(m, 'click', () => {
       mapRef.current?.morph(m.getPosition(), 18, {
         duration: 1000,
@@ -79,26 +73,21 @@ const addMarkers = (
   });
 
   // const clustering = initCluster(markerObjectList, mapRef.current!);
-  /*
-   * todo : naver cloud api map forum에서 클러스터별 최상단 마커의 종류에 따른 (클러스터 아이콘) 설정이 가능하다고 답변받을시
+  /* todo : naver cloud api map forum에서 클러스터별 최상단 마커의 종류에 따른 (클러스터 아이콘) 설정이 가능하다고 답변받을시
    * clustering.setIcons([clusterIconList[카테고리]])를 사용하여 클러스터 아이콘 지정 구현
    */
 };
 
-// 지도에 표시된 마커 객체 리스트를 삭제하는 함수
-const deleteMarkers = (
-  markerObjectList: naver.maps.Marker[]
-  // clearMarkerObjectList: () => void
-) => {
-  if (markerObjectList.length > 0) {
-    // boundsRef.current = null;
-    markerObjectList.forEach((m) => {
-      m.setMap(null);
-    });
-    // clearMarkerObjectList();
-  }
+/* 마커 제거 함수 */
+const deleteMarkers = (objectList: naver.maps.Marker[] | null) => {
+  if (!objectList) return;
+
+  objectList.forEach((m) => {
+    m.setMap(null);
+  });
 };
 
+/* // 클러스터 생성 함수
 const initCluster = (markerArray: naver.maps.Marker[], map: naver.maps.Map) => {
   const markerClustering = new MarkerClustering({
     map: map,
@@ -117,147 +106,138 @@ const initCluster = (markerArray: naver.maps.Marker[], map: naver.maps.Map) => {
 
   return markerClustering;
 };
+*/
 
+/* MapSheet.tsx */
 const MapSheet = () => {
   const mapElement = useRef<HTMLDivElement | null>(null);
   const mapInstance = useRef<naver.maps.Map | null>(null);
-  const boundsRef = useRef<naver.maps.LatLngBounds>(null);
 
-  const { diplayLatLng, setDisplayLatLng } = useMapStore(
+  const {
+    initCenter,
+    isSearchBounds,
+    setIsSearchBounds,
+    lastBounds,
+    setLastBounds,
+    lastZoom,
+    setLastZoom,
+  } = useMapStore(
     useShallow((state) => ({
-      diplayLatLng: state.diplayLatLng,
-      setDisplayLatLng: state.setDisplayLatLng,
+      initCenter: state.initCenter,
+      setInitCenter: state.setInitCenter,
+      isSearchBounds: state.isSearchBounds,
+      setIsSearchBounds: state.setIsSearchBounds,
+      lastBounds: state.lastBounds,
+      setLastBounds: state.setLastBounds,
+      lastZoom: state.lastZoom,
+      setLastZoom: state.setLastZoom,
     }))
   );
 
-  const { markerObjectList, setMarkerObjectList } = useMarkersStore(
-    useShallow((state) => ({
-      markerObjectList: state.markerObjectList,
-      setMarkerObjectList: state.setMarkerObjectList,
-    }))
-  );
+  const { newMarkerObjectList, setNewMarkerObjectList, prevMarkerObjectList } =
+    useMarkerStore(
+      useShallow((state) => ({
+        newMarkerObjectList: state.newMarkerObjectList,
+        setNewMarkerObjectList: state.setNewMarkerObjectList,
+        prevMarkerObjectList: state.prevMarkerObjectList,
+        setPrevMarkerObjectList: state.setPrevMarkerObjectList,
+      }))
+    );
 
-  const { data, error, isError } = useQuery({
-    queryKey: ['displayMarkers', diplayLatLng],
-    queryFn: () =>
-      getDisplayMarkers(
-        diplayLatLng!.swX,
-        diplayLatLng!.swY,
-        diplayLatLng!.neX,
-        diplayLatLng!.neY
-      ),
-  });
-
-  if (isError) {
-    console.log('fetchPlacesNearby isError :::', error);
-  }
-
-  // 지도를 생성하고 화면 좌표를 설정함
   useLayoutEffect(() => {
     if (!mapElement.current) return;
 
-    getUserLatLng().then((userLatLng) => {
-      initMap(mapElement, mapInstance, userLatLng);
-      const bounds = getDisplayBounds(mapInstance.current);
-      setDisplayLatLng({
-        swY: bounds.minY(),
-        swX: bounds.minX(),
-        neY: bounds.maxY(),
-        neX: bounds.maxX(),
-      });
-    });
-  }, []);
-
-  // useMarkerStore의 markerObjectList를 구독하여 변경이 감지될 때마다 작업을 수행하도록함
-  useEffect(() => {
-    const unsubscribe = useMarkersStore.subscribe(
-      (state) => state.markerObjectList,
-      (current, prev) => {
-        console.log('current :::', current);
-        if (current.length > 0) {
-          console.log('prev :::', prev);
-          deleteMarkers(prev);
-          addMarkers(mapInstance, current);
-        }
-      }
+    // 지도 생성
+    const center = new naver.maps.LatLng(initCenter.lat, initCenter.lng);
+    const map = initMap(
+      mapElement,
+      mapInstance,
+      center,
+      isSearchBounds,
+      lastBounds,
+      lastZoom
     );
 
-    return () => unsubscribe();
-  }, []);
+    if (!map) return;
+    if (isSearchBounds) setIsSearchBounds(false);
 
-  // useQuery의 getDisplayMarkers 호출 성공시 받아온 마커 리스트를 마커 객체 리스트로 생성하여 markerObjectList에 한 번에 저장함
-  useEffect(() => {
-    if (data) {
-      const markerList: naver.maps.Marker[] = [];
-      data.map((place: MarkersInfoType) => {
-        const position = new naver.maps.LatLng(place.latitude, place.longitude);
-        const icon = IconMarkerMap[place.category];
-        const marker = new naver.maps.Marker({
-          position: position,
-          icon: {
-            url: icon,
-          },
-        });
-        markerList.push(marker);
+    // 이벤트 리스너 추가
+    const idleEventListener = naver.maps.Event.addListener(map, 'idle', () => {
+      const idleBounds = map.getBounds();
+      const idleZoom = map.getZoom();
+      setLastBounds(idleBounds);
+      setLastZoom(idleZoom);
+    });
+
+    const newBounds = map.getBounds();
+
+    // 추가할 마커 객체가 없으면 getDisplayMarkers api 호출
+    if (!newMarkerObjectList) {
+      getDisplayMarkers(
+        newBounds.getMin().x,
+        newBounds.getMin().y,
+        newBounds.getMax().x,
+        newBounds.getMax().y
+      ).then((res) => {
+        const newObjects = createMarkerObjectList(res);
+        setNewMarkerObjectList(newObjects);
       });
-      setMarkerObjectList(markerList);
     }
-  }, [data]);
 
-  // 임시 버튼: 표시된 마커 기준으로 지도 이동
-  // const moveToMarkers = useCallback(() => {
-  //   if (!mapInstance.current || !boundsRef.current) return;
-
-  //   mapInstance.current.panToBounds(
-  //     boundsRef.current,
-  //     {
-  //       duration: 1000,
-  //       easing: 'easeOutCubic',
-  //     },
-  //     {
-  //       top: 30,
-  //       right: 30,
-  //       bottom: 200,
-  //       left: 30,
-  //     }
-  //   );
-  // }, []);
-
-  const resetCurrentDisplayBounds = useCallback(() => {
-    const bounds = getDisplayBounds(mapInstance.current!);
-    setDisplayLatLng({
-      swY: bounds.minY(),
-      swX: bounds.minX(),
-      neY: bounds.maxY(),
-      neX: bounds.maxX(),
-    });
+    return () => {
+      naver.maps.Event.removeListener(idleEventListener);
+      map.destroy();
+    };
   }, []);
 
-  const moveToCurrentLocation = useCallback(() => {
-    if (!mapInstance.current) {
-      console.warn('mapInstance.current가 null입니다!');
-      return;
-    }
+  // useEffect(() => {
+  //   console.log('lastBounds:', lastBounds);
+  //   console.log('getZoom:', mapInstance.current?.getZoom());
+  // }, [lastBounds]);
 
-    getUserLatLng().then((userLatLng) => {
-      mapInstance.current?.panTo(userLatLng);
-    });
+  /* [useEffect] prevMarkerObjectList 변경될 때 */
+  useEffect(() => {
+    deleteMarkers(prevMarkerObjectList);
+  }, [prevMarkerObjectList]);
+
+  /* [useEffect] newMarkerObjectList 변경될 때 */
+  useEffect(() => {
+    if (!mapInstance.current) return;
+
+    // 지도에 마커 추가
+    addMarkers(mapInstance, newMarkerObjectList);
+  }, [newMarkerObjectList]);
+
+  /* 현재 지도 화면을 기준으로 마커 재검색 함수 */
+  const researchMarker = useCallback(async () => {
+    if (!mapInstance.current) return;
+
+    const currentBounds = mapInstance.current.getBounds();
+    const data = await getDisplayMarkers(
+      currentBounds.getMin().x,
+      currentBounds.getMin().y,
+      currentBounds.getMax().x,
+      currentBounds.getMax().y
+    );
+
+    const newObjects = createMarkerObjectList(data);
+    setNewMarkerObjectList(newObjects);
+  }, []);
+
+  /* 실시간 사용자 위치로 지도 이동 */
+  const moveToUserLocation = useCallback(async () => {
+    if (!mapInstance.current) return;
+
+    const { lat, lng } = await getUserLocation();
+    mapInstance.current.panTo(new naver.maps.LatLng(lat, lng));
   }, []);
 
   return (
     <div className='relative w-dvw h-dvh'>
       <div ref={mapElement} className='w-full h-full' />
 
-      {/* 임시 버튼: 표시된 마커 기준으로 지도 이동*/}
-      {/* <button
-        onClick={moveToMarkers}
-        className='absolute left-4 top-1/2 -translate-y-1/2 bg-white shadow px-4 py-2 rounded text-sm z-10'>
-        표시된 마커로 지도 이동
-      </button> */}
-
-      <ReloadMarkerButton handleClick={resetCurrentDisplayBounds} />
-
-      <CurrentLocationButton handleClick={moveToCurrentLocation} />
+      <ReloadMarkerButton handleClick={researchMarker} />
+      <CurrentLocationButton handleClick={moveToUserLocation} />
     </div>
   );
 };
