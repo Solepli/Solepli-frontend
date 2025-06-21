4329c613cdb36df9897fbe2c1fa92733feb5519e
sky121016 <sky121016@naver.com>
2025-06-21 16:48:06 +0900

    [feat/#129] 검색결과 있지만 카테고리 필터링 결과 없으면 SollectChipList 보이게 수정

--- src/pages/SollectSearchResultPage.tsx
+++ src/pages/SollectSearchResultPage.tsx
@@ -12,6 +12,7 @@ import { useScrollSentinel } from '../hooks/useInfiniteScrollQuery';
 const SollectSearchResultPage = () => {
   const { inputValue } = useSearchStore();
   const { selectedCategory, clearCategory } = useSollectStore();
+  const [hasResult, setHasResult] = useState(false);
 
   // useInfiniteQuery를 통한 무한 스크롤 구현
   // pageParam이 cursorId 역할을 한다. 초기값은 undefined
@@ -39,12 +40,22 @@ const SollectSearchResultPage = () => {
     isFetchingNextPage,
   });
 
+  // 프론트에서 카테고리 필터링 하지 않고 백엔드에서 그냥 처리하도록 함
+  // 검색 결과 데이터가 처음에 있었으면 (한번이라도 존재했으면) hasResult true
+  // 검색 결과 데이터가 처음부터 없었으면 (한 번도 존재 안했으면) hasResult false
+  // hasResult로 SollectChipList 띄울지 말지 결정
+  useEffect(()=>{
+    if (sollects && sollects.length !== 0) {
+      setHasResult(true);
+    }
+  },[data]);
+
 
   return (
     <div>
       <div className='z-10 w-full bg-white fixed'>
         <SollectGNB />
-        {sollects && sollects.length !== 0 && <SollectChipList />}
+        {hasResult && <SollectChipList />}
       </div>
 
       {sollects && sollects.length !== 0 ? (
@@ -54,6 +65,10 @@ const SollectSearchResultPage = () => {
           {isFetchingNextPage && (
             <div className='text-center py-4'>로딩 중...</div>
           )}
+        </div>
+      ) : hasResult ? (
+        <div className='pt-61'>
+          <SollectNoResult />
         </div>
       ) : (
         <SollectNoResult />
