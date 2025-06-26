9054c9f9692dc7ccf0df0500a8cef8f770d397ef
lth-1026 <th20001026@gmail.com>
2025-06-26 18:50:58 +0900

    [feat/#119] hello-pangea/dnd 를 이용해 추가된 장소 리스트를 drag and drop으로 순서 변경
    여러 dnd 라이브러리가 존재하는데, 쉽고 빠르게 구현할 수 있는 hello-pangea/dnd를 선택함.
    아래의 링크를 참조해 해당 기능을 구현함.
    기존 컴포넌트를 Draggable 컴포넌트가 감싸서 drag할 수 있게 만듦.
    추가적으로 기존에 Css active를 이용해 선택한 컴포넌트에 대해 변경된 배경색을 적용했는데,
    해당 라이브러리에서 제공하는 isDragging 을 이용해 선택된 컴포넌트의 배경색을 변경하는 기능으로 변경함.
    기존 active 가 작동하지 않아서 변경함.
    
    https://codesandbox.io/p/sandbox/k260nyxq9v?file=%2Findex.js%3A17%2C1
    
    https://github.com/hello-pangea/dnd

--- src/components/Sollect/SollectWrite/SollectWriteAddedPlace.tsx
+++ src/components/Sollect/SollectWrite/SollectWriteAddedPlace.tsx
@@ -4,25 +4,56 @@ import Trash from '../../../assets/trash.svg?react';
 import { ReleatedSearchPlace } from '../../../types';
 import { iconSmallMap } from '../../../utils/icon';
 
-const SollectWriteAddedPlace: React.FC<{ place: ReleatedSearchPlace }> = ({ place }) => {
-    return (
-        <div className="group flex items-center justify-center w-full h-60 py-12 pl-2 pr-6 bg-primary-50 rounded-lg border border-primary-100 gap-2
-        active:bg-primary-500 active:border-primary-100 active:text-primary-50">
-            <DragAndDrap className='w-24 h-24 flex-shrink-0 text-primary-400 group-active:text-primary-50' />
-            <div className='flex-1 flex gap-10 items-center h-35'>
-                {place.category && iconSmallMap[place.category] && (
-                    React.createElement(iconSmallMap[place.category], {className: 'w-32 h-32 flex-shrink-0'})
-                )}
-                <div className='flex flex-col justify-between h-full'>
-                    <span className='text-primary-950 text-sm font-semibold leading-tight group-active:text-primary-50'> {place.name}</span>
-                    <span className='text-primary-400 text-xs font-normal leading-none group-active:text-primary-50'>{place.address}</span>
-                </div>
-            </div>
-            <div className='p-6'>
-                <Trash className='w-24 h-24 flex-shrink-0 text-primary-400 group-active:text-primary-50' />
-            </div>
+const SollectWriteAddedPlace: React.FC<{
+  place: ReleatedSearchPlace;
+  isDragging?: boolean;
+}> = ({ place, isDragging = false }) => {
+  return (
+    <div
+      className={`group flex items-center justify-center w-full h-60 py-12 pl-2 pr-6 rounded-lg border gap-2
+          ${
+            isDragging
+              ? 'bg-primary-500 border-primary-100 text-primary-50'
+              : 'bg-primary-50 border-primary-100'
+          }
+          `}>
+      <DragAndDrap
+        className={`w-24 h-24 flex-shrink-0 ${
+          isDragging ? 'text-primary-50' : 'text-primary-400'
+        }`}
+      />
+      <div className='flex-1 flex gap-10 items-center h-35'>
+        {place.category &&
+          iconSmallMap[place.category] &&
+          React.createElement(iconSmallMap[place.category], {
+            className: `w-32 h-32 flex-shrink-0 ${
+              isDragging ? 'text-primary-50' : 'text-current'
+            }`,
+          })}
+        <div className='flex flex-col justify-between h-full'>
+          <span
+            className={`text-sm font-semibold leading-tight ${
+              isDragging ? 'text-primary-50' : 'text-primary-950'
+            }`}>
+            {place.name}
+          </span>
+          <span
+            className={`text-xs font-normal leading-none ${
+              isDragging ? 'text-primary-50' : 'text-primary-400'
+            }`}>
+            {place.address}
+          </span>
         </div>
-    );
-}
+      </div>
+      <div className='p-6'>
+        <Trash
+          className={`w-24 h-24 flex-shrink-0 ${
+            isDragging ? 'text-primary-50' : 'text-primary-400'
+          }`}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default SollectWriteAddedPlace;
