22603a0f47f0137199b9c6f772a438fb4423db11
lth-1026 <th20001026@gmail.com>
2025-06-27 00:43:26 +0900

    [feat/#145] Toast 기능 추가
    Toast 라이브러리인 toastify를 추가했음.
    최상단 파일인 main.tsx에 ToastConainer를 솔플리에 맞게 설정해 선언함.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './routes/AppRouter.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ToastContainer } from 'react-toastify';
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,16 @@ createRoot(document.getElementById('root')!).render(
         <AppRouter />
       </BrowserRouter>
     </QueryClientProvider>
+    <ToastContainer 
+    position='bottom-center'
+    autoClose={2000}
+    hideProgressBar={true}
+    newestOnTop={true}
+    closeOnClick={false}
+    closeButton={false}
+    toastStyle={{ background: 'transparent', boxShadow: 'none', }}
+    style={{ bottom: '3rem',}}
+    />
   </>
   // </StrictMode>,
 );
