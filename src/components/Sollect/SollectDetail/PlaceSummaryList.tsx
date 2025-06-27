ddda9bd0dd8bac8ab0b0e28b0003a304fa2634e0
sky121016 <sky121016@naver.com>
2025-06-28 00:32:36 +0900

    [feat/#146] 쏠렉트 디테일 페이지에서 쓰일 PlaceSummaryList 생성

---
+++ src/components/Sollect/SollectDetail/PlaceSummaryList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useSollectDetailStore } from '../../../store/sollectDetailStore'
+import PreviewContentSummary from '../../Place/PreviewContentSummary';
+
+const PlaceSummaryList = () => {
+    const {placeSummaries} = useSollectDetailStore();
+
+  return <div>{placeSummaries.map((place, index)=>{
+    return(
+        <PreviewContentSummary key={index} place={place}/>
+    )
+  })}</div>;
+}
+
+export default PlaceSummaryList
