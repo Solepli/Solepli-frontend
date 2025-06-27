0ef8c9258472922684194f30cd6614fb0af74493
sky121016 <sky121016@naver.com>
2025-06-27 18:02:47 +0900

    [feat/#146] 수정, 삭제 메뉴 버튼 구현

--- src/components/Sollect/SollectDetail/SollectDetailHeader.tsx
+++ src/components/Sollect/SollectDetail/SollectDetailHeader.tsx
@@ -1,21 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import arrowTail from '../../../assets/arrowTail.svg';
 import arrowTailWhite from '../../../assets/arrowTailWhite.svg';
+import kebabWhite from '../../../assets/kebabWhite.svg';
+import kebabGray from '../../../assets/kebabGray.svg';
+import edit from '../../../assets/edit.svg';
+import deleteIcon from '../../../assets/delete.svg';
 
 import { useNavigate } from 'react-router-dom';
 
 const SollectDetailHeader = ({ isTop }: { isTop: boolean }) => {
   const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+  const mySollect = true;
 
   return (
     <div
-      className={`w-full h-54 flex items-center fixed z-10 pl-9 ${isTop ? 'bg-transparent' : 'bg-white'} transition-colors duration-300`}
-      onClick={() => navigate(-1)}>
+      className={`w-full h-54 flex justify-between items-center fixed z-10 px-9 ${isTop ? 'bg-transparent' : 'bg-white'} transition-colors duration-300`}>
+      {/* 뒤로가기 */}
       <img
         src={isTop ? arrowTailWhite : arrowTail}
         alt='arrowTail'
-        className='rotate-180 w-30 h-30'
+        className='rotate-180 w-24 h-24'
+        onClick={() => navigate(-1)}
       />
+
+      {mySollect && (
+        <div>
+          {/* 메뉴 */}
+          <img
+            src={isTop ? kebabWhite : kebabGray}
+            alt='kebab'
+            className='w-24 h-24'
+            onClick={() => setShowMenu(!showMenu)}
+          />
+
+          {showMenu && (
+            <div
+              className='absolute top-40 right-12 rounded-lg border-1 border-primary-100 bg-white w-136 shadow-[3px_3px_4px_0px_rgba(24,24,24,0.06)] text-primary-950
+text-sm'>
+              {/* 수정 */}
+              <div className='p-12 flex gap-8 items-center border-b border-primary-100'>
+                <img src={edit} alt='edit' />
+                수정
+              </div>
+              {/* 삭제 */}
+              <div className='p-12 flex gap-8 items-center'>
+                <img src={deleteIcon} alt='delete' />
+                삭제
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
