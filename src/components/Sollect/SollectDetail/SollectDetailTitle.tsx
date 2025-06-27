272dbcf5191c7500e2fda62f7aa0011088a86e93
sky121016 <sky121016@naver.com>
2025-06-27 16:06:28 +0900

    [feat/#146] Title 구현

---
+++ src/components/Sollect/SollectDetail/SollectDetailTitle.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useSollectDetailStore } from '../../../store/sollectDetailStore';
+import locationWhite from '../../../assets/locationWhite.svg';
+
+const SollectDetailTitle = () => {
+  const sollect = useSollectDetailStore();
+  console.log(sollect);
+  return (
+    <div>
+      <div
+        style={{
+          backgroundImage: `url(${sollect.thumbnailImageUrl})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundColor: sollect.thumbnailImageUrl ? '#18181866' : '#373737',
+        }}
+        className='w-full h-232 flex flex-col justify-end p-16 text-white'>
+        <h1 className='text-2xl font-bold'>{sollect.title}</h1>
+        <div className='text-xs flex justify-between'>
+          <span className='text-xs'>
+            {' '}
+            {sollect.placeName}
+            {sollect.otherPlaceCount > 0 && `(외 ${sollect.otherPlaceCount}곳)`}
+          </span>
+          <span className='text-xs flex items-center'>
+            <img src={locationWhite} alt='location' className='h-16 w-16' />
+            {sollect.district}, {sollect.neighborhood}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SollectDetailTitle;
