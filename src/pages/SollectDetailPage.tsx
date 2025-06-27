059fcf724e068be10ea27b5d9329a5ea9d0db698
sky121016 <sky121016@naver.com>
2025-06-27 15:59:42 +0900

    [feat/#146] 쏠렉트 디테일 조회 페이지 생성

--- src/pages/SollectDetailPage.tsx
+++ src/pages/SollectDetailPage.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import SollectDetailHeader from '../components/Sollect/SollectDetail/SollectDetailHeader';
+import { useQuery } from '@tanstack/react-query';
+import { useLocation } from 'react-router-dom';
+import { fetchSollectDetail } from '../api/sollectApi';
+import { useSollectDetailStore } from '../store/sollectDetailStore';
+import SollectDetailTitle from '../components/Sollect/SollectDetail/SollectDetailTitle';
+import SollectDetailProfile from '../components/Sollect/SollectDetail/SollectDetailProfile';
+import SollectDetailContent from '../components/Sollect/SollectDetail/SollectDetailContent';
 
 const SollectDetailPage = () => {
+  const sollectId = 4;
+  const { setSollectDetail } = useSollectDetailStore();
+
+  const sollect = useQuery({
+    queryKey: ['sollectDetail', sollectId],
+    queryFn: () => fetchSollectDetail(sollectId),
+    enabled: !!sollectId,
+  });
+
+  useEffect(() => {
+    if (sollect.data) {
+      setSollectDetail(sollect.data);
+    }
+  }, [sollect.data, setSollectDetail]);
   return (
     <div>
-        
-    </div>
-  )
-}
+      {/* SollectDetailHeader */}
+      <SollectDetailHeader />
 
-export default SollectDetailPage
+      {/* Title */}
+      <SollectDetailTitle />
+
+      <div className='flex flex-col'>
+        {/* Profile */}
+        <SollectDetailProfile />
+
+        {/* content */}
+        <SollectDetailContent />
+      </div>
+      {/* Bottom Bar */}
+    </div>
+  );
+};
+
+export default SollectDetailPage;
