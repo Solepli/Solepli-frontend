f8662a9ad9027bcfb99ca49ff63ee81f05313126
sky121016 <sky121016@naver.com>
2025-06-28 23:28:48 +0900

    [refactor/#151] place mock 데이터 삭제

--- src/api/placeApi.ts
+++ src/api/placeApi.ts
@@ -3,10 +3,7 @@ import { publicAxios } from './axios';
 import qs from 'qs';
 import { ENDPOINT } from './urls';
 
-export const fetchPlaces = async () => {
-  await new Promise((res) => setTimeout(res, 500));
-  return mockPlaces;
-};
+
 
 export const getPlacesByDisplay = async (
   swLat: number, // swY - min
@@ -70,7 +67,7 @@ export const getPlaceByIdList = async (
 
 export const getPlaceDetail = async (id: number) => {
   const res = await publicAxios.get(ENDPOINT.SOLMAP_PLACE_DETAIL + id);
-
+  
   return res.data.data;
 };
 
@@ -88,104 +85,3 @@ export const getPlacesNearby = async (
   return res.data.data;
 };
 
-export const fetchPlaceById = async (id: number) => {
-  // await new Promise((res) => setTimeout(res, 300));
-  return mockPlaces[id];
-};
-
-const mockPlaces: Place[] = [
-  {
-    id: 1,
-    title: '스타벅스 노량진동점',
-    address: '서울 동작구 노량진로 190',
-    latitude: 37.513213,
-    longitude: 126.94656,
-    category: { title: '카페', id: 'cafe' },
-    rating: 4.5,
-    tags: [
-      { id: 'quiet', text: '조용한' },
-      { id: 'lively', text: '시끌벅적한' },
-      { id: 'cozy', text: '편안한' },
-    ],
-    hours: [
-      { day: 0, startTime: '10:00', endTime: '20:30' },
-      { day: 1, startTime: '10:00', endTime: '20:30' },
-      { day: 2, startTime: '10:00', endTime: '20:30' },
-      { day: 3, startTime: '10:00', endTime: '20:30' },
-      { day: 4, startTime: '10:00', endTime: '20:30' },
-      { day: 5, startTime: '10:00', endTime: '20:30' },
-      { day: 6, startTime: '10:00', endTime: '17:00' },
-    ],
-  },
-
-  {
-    id: 2,
-    title: '도로도로커피숍',
-    address: '서울 동작구 노량진로 145 1층',
-    latitude: 37.51388,
-    longitude: 126.942058,
-    category: { title: '카페', id: 'cafe' },
-    rating: 4.2,
-    tags: [
-      { id: 'quiet', text: '조용한' },
-      { id: 'lively', text: '시끌벅적한' },
-      { id: 'cozy', text: '편안한' },
-    ],
-    hours: [
-      { day: 0, startTime: '10:00', endTime: '20:30' },
-      { day: 1, startTime: '10:00', endTime: '20:30' },
-      { day: 2, startTime: '10:00', endTime: '20:30' },
-      { day: 3, startTime: '10:00', endTime: '20:30' },
-      { day: 4, startTime: '10:00', endTime: '20:30' },
-      { day: 5, startTime: '10:00', endTime: '20:30' },
-      { day: 6, startTime: '10:00', endTime: '17:00' },
-    ],
-  },
-  {
-    id: 3,
-    title: '미분당',
-    address: '서울 동작구 노량진로 190',
-    latitude: 37.5132,
-    longitude: 126.946,
-    category: { title: '식당', id: 'food' },
-    rating: 4.5,
-    tags: [
-      { id: 'luxurious', text: '고급스러운' },
-      { id: 'unique', text: '색다른' },
-      { id: 'hip', text: '힙한' },
-    ],
-    hours: [
-      { day: 0, startTime: '10:00', endTime: '20:30' },
-      { day: 1, startTime: '10:00', endTime: '20:30' },
-      { day: 2, startTime: '10:00', endTime: '20:30' },
-      { day: 3, startTime: '10:00', endTime: '20:30' },
-      { day: 4, startTime: '10:00', endTime: '20:30' },
-      { day: 5, startTime: '10:00', endTime: '20:30' },
-      { day: 6, startTime: '10:00', endTime: '17:00' },
-    ],
-  },
-
-  {
-    id: 4,
-    title: '샤브로',
-    address: '서울 동작구 노량진로 145 1층',
-    latitude: 37.513,
-    longitude: 126.942,
-    category: { title: '식당', id: 'food' },
-    rating: 4.2,
-    tags: [
-      { id: 'luxurious', text: '고급스러운' },
-      { id: 'unique', text: '색다른' },
-      { id: 'hip', text: '힙한' },
-    ],
-    hours: [
-      { day: 0, startTime: '10:00', endTime: '20:30' },
-      { day: 1, startTime: '10:00', endTime: '20:30' },
-      { day: 2, startTime: '10:00', endTime: '20:30' },
-      { day: 3, startTime: '10:00', endTime: '20:30' },
-      { day: 4, startTime: '10:00', endTime: '20:30' },
-      { day: 5, startTime: '10:00', endTime: '20:30' },
-      { day: 6, startTime: '10:00', endTime: '17:00' },
-    ],
-  },
-];
