7c851be86f8e218960ec22da745de84f6cdfd559
sky121016 <sky121016@naver.com>
2025-06-30 22:39:37 +0900

    [feat/#160] 쏠마크 장소 디테일 조회 api 적용

--- src/pages/SolmarkPlacePreviewPage.tsx
+++ src/pages/SolmarkPlacePreviewPage.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import TitleHeader from '../components/global/TitleHeader'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { fetchPlacesByCollectionId } from '../api/solmarkApi';
 
 const SolmarkPlacePreviewPage = () => {
     const navigate = useNavigate();
+    const {collectionId} = useParams();
+
     const handleClick = () =>{
         navigate(-1);
     }
+
+    const { data } = useQuery({
+      queryKey: ['solmarkPlaces', collectionId],
+      queryFn: () => fetchPlacesByCollectionId(Number(collectionId)),
+    });
+
   return (
     <div>
         <TitleHeader title='성수동 모음' center onClick={handleClick}/>
+        <p>장소 </p>
     </div>
   )
 }
