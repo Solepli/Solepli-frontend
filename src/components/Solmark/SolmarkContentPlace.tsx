16b416ea7f241664b90016829ae2b81804319e7b
sky121016 <sky121016@naver.com>
2025-06-30 22:38:48 +0900

    [feat/#160] list 데이터 불러오기

--- src/components/Solmark/SolmarkContentPlace.tsx
+++ src/components/Solmark/SolmarkContentPlace.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import SolmarkPlaceList from './SolmarkPlaceList'
+import React, { useEffect, useState } from 'react'
+import SolmarkPlaceList from './SolmarkPlaceListCard'
+import { useQuery } from '@tanstack/react-query'
+import { fetchPlaceCollections } from '../../api/solmarkApi'
 
 const SolmarkContentPlace = () => {
+    const {data} = useQuery({
+    queryKey:['solmarkPlaceList'],
+    queryFn:()=>fetchPlaceCollections(),
+  })
+  const [listData, setListData] = useState([]);
+
+  useEffect(()=>{
+    if(data){
+      setListData(data);
+    }
+  },[data]);
+  
   return (
     <div className='py-24 px-16 flex flex-col gap-12'>
-        <SolmarkPlaceList/>
+        {listData && listData.map((list)=>{
+          return <SolmarkPlaceList list={list} />;
+        })}
     </div>
   )
 }
