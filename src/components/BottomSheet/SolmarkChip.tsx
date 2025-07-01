a7cb45a50e46889cf52f89fae18141f4eef2ae4a
sky121016 <sky121016@naver.com>
2025-07-01 13:02:11 +0900

    [feat/#170] 쏠마크칩 색상 변경

--- src/components/BottomSheet/SolmarkChip.tsx
+++ src/components/BottomSheet/SolmarkChip.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 
 import heart from '../../assets/heart.svg';
-import heartFill from '../../assets/heartFill.svg';
+import heartFillWhite from '../../assets/heartFillWhite.svg';
 import { usePlaceStore } from '../../store/placeStore';
 import { patchSolmark } from '../../api/solmarkApi';
 
 interface SolmarkChipProps {
   label?: boolean;
   markCount?: number;
+  isMarked?:boolean;
   placeId: number;
 }
 
@@ -16,11 +17,20 @@ const SolmarkChip: React.FC<SolmarkChipProps> = ({
   placeId,
   label,
   markCount,
+  isMarked
 }) => {
   const { selectedPlace } = usePlaceStore();
   const [isSolmark, setIsSolmark] = useState(
     label ? selectedPlace?.isMarked : false
   );
+  const [count, setCount] = useState(markCount ? markCount : 0);
+
+  useEffect(()=>{
+    if(isMarked){
+      setIsSolmark(true);
+    }
+  },[]) 
+
 
   const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation(); // 쏠마크칩 클릭 시 navigate 방지
@@ -32,8 +42,10 @@ const SolmarkChip: React.FC<SolmarkChipProps> = ({
 
     if (isSolmark) {
       patchSolmark(placeId, empty, array);
+      setCount(count-1);
     } else {
       patchSolmark(placeId, array, empty);
+      setCount(count+1);
     }
   };
 
@@ -42,23 +54,23 @@ const SolmarkChip: React.FC<SolmarkChipProps> = ({
       {label ? (
         <div
           className={`flex w-58 h-32 p-2 border rounded-lg justify-center items-center 
-        ${isSolmark ? 'border-chip-mark' : 'border-chip-bg-mark'}`}>
+        ${isSolmark ? 'border-chip-mark bg-chip-mark' : 'border-chip-bg-mark'}`}>
           <img
-            src={isSolmark ? heartFill : heart}
+            src={isSolmark ? heartFillWhite : heart}
             alt=''
             className='w-24 h-24'
           />
           <p
-            className={`w-30 text-center text-xs ${isSolmark ? 'text-chip-mark' : 'text-chip-bg-mark'}`}>
-            {markCount}
+            className={`w-30 text-center text-xs ${isSolmark ? 'text-white' : 'text-chip-bg-mark'}`}>
+            {count}
           </p>
         </div>
       ) : (
         <div
           className={`flex w-24 h-24 border rounded-lg justify-center items-center 
-        ${isSolmark ? 'border-chip-mark' : 'border-chip-bg-mark'}`}>
+        ${isSolmark ? 'bg-chip-mark border-chip-mark' : 'border-chip-bg-mark'}`}>
           <img
-            src={isSolmark ? heartFill : heart}
+            src={isSolmark ? heartFillWhite : heart}
             alt=''
             className='w-24 h-24'
           />
