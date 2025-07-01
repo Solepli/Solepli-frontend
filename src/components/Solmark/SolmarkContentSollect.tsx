aca0cba535b1a9fe876867e981a218c7c322cd06
sky121016 <sky121016@naver.com>
2025-07-01 17:33:44 +0900

    [feat/#170] 쏠마크 쏠렉트 noresult 페이지

--- src/components/Solmark/SolmarkContentSollect.tsx
+++ src/components/Solmark/SolmarkContentSollect.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { fetchSolmarkSollect } from '../../api/solmarkApi';
 import SollectList from '../Sollect/SollectList';
 import { SollectPhotoType } from '../../types';
+import arrow from '../../assets/arrow.svg';
 
 const SolmarkContentSollect = () => {
   const { data } = useQuery({
@@ -20,7 +21,21 @@ const SolmarkContentSollect = () => {
 
   return (
     <div className='py-16'>
-      {sollects.length !== 0 ? <SollectList sollects={data} /> : <div>no</div>}
+      {sollects.length !== 1 ? (
+        <SollectList sollects={data} />
+      ) : (
+        <div className='flex justify-center items-center h-full text-center'>
+          <div className='pt-240'>
+            <p className='font-bold text-primary-950'>
+              아직 저장된 쏠렉트가 없어요!
+            </p>
+            <p className='flex text-center justify-center'>
+              쏠렉트 보러 가기
+              <img src={arrow} alt='' className='w-24 h-24' />{' '}
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
