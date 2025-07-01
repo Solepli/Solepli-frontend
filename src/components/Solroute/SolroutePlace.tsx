3f2962575350eb48f03a3c9b250bad6bb3a57b52
ion <delion.dev@gmail.com>
2025-07-01 21:06:26 +0900

    [feat/#172] SolroutePlace 좌측 점선 및 드래그 앤 드롭 핸들 UI 구현
    SolroutePlace 컴포넌트에 좌측 점선을 SVG로 배경 반복 표시하여
    단계 구분을 시각적으로 나타내고, 드래그 앤 드롭 핸들 UI를 추가함.
    
    처음에 src 폴더에 SVG를 두고 불러오니 반복되지 않는 오류가 있었는데,
    최적화 과정에서 width, height 속성이 제거되어 발생한 것으로 추정됨.
    이를 public 폴더에 위치시켜 background-repeat 속성이 정상적으로 작동하도록 해결함.

--- src/components/Solroute/SolroutePlace.tsx
+++ src/components/Solroute/SolroutePlace.tsx
@@ -1,8 +1,18 @@
+import DragAndDropLine from '../../assets/dragAndDropLine.svg?react';
+
 const SolroutePlace = () => {
   return (
     <div className='flex items-start self-stretch'>
       <div className='flex pl-20 items-start gap-16 flex-[1_0_0]'>
-        <div></div>
+        <div className='flex w-9 flex-col justify-center items-center gap-4 self-stretch'>
+          <img src={'/solrouteLineDash.svg'} alt='SolrouteLineDash' />
+          <div className='flex w-24 h-24 flex-col justify-center items-center gap-10 aspect-square rounded-[8px] bg-secondary-700'>
+            <DragAndDropLine />
+          </div>
+          <div
+            className={`min-h-111 self-stretch bg-repeat-y bg-top bg-[url("/solrouteLineDash.svg")]`}
+          />
+        </div>
         <div></div>
       </div>
     </div>
