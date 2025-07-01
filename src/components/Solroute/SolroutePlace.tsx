00319fd71e93aace9ea7db72548aba48bb723b06
ion <delion.dev@gmail.com>
2025-07-01 18:23:26 +0900

    [feat/#172] SolroutePlace 컴포넌트 생성

---
+++ src/components/Solroute/SolroutePlace.tsx
@@ -0,0 +1,12 @@
+const SolroutePlace = () => {
+  return (
+    <div className='flex items-start self-stretch'>
+      <div className='flex pl-20 items-start gap-16 flex-[1_0_0]'>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default SolroutePlace;
