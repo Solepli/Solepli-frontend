1d0bb2da197739c57dbbef251daa8517aa5f2934
ion <delion.dev@gmail.com>
2025-07-01 23:46:13 +0900

    [feat/#172] SolroutePlace 요소를 SolrouteWritePage.tsx에 연결

--- src/pages/SolrouteWritePage.tsx
+++ src/pages/SolrouteWritePage.tsx
@@ -1,7 +1,10 @@
 import SollectWriteHeader from '../components/Sollect/SollectWrite/SollectWriteHeader';
 import SolrouteMap from '../components/Solroute/SolrouteMap';
+import SolroutePlace from '../components/Solroute/SolroutePlace';
 import SolroutePlaceAddButton from '../components/Solroute/SolroutePlaceAddButton';
 import SolrouteTitle from '../components/Solroute/SolrouteTitle';
+
+const examples = [0, 1, 2, 3, 4, 5];
 
 const SollectWritePage = () => {
   return (
@@ -17,6 +20,14 @@ const SollectWritePage = () => {
       <div className='flex-1 overflow-y-auto mt-50'>
         <SolrouteTitle />
         <SolrouteMap />
+        <div className='flex pt-24 pb-8 pl-16 items-center gap-10 self-stretch'>
+          <p className='text-primary-950 text-sm not-italic font-normal leading-[150%] tracking-[-0.35px]'>
+            장소 {examples.length}개
+          </p>
+        </div>
+        {examples.map((e) => {
+          return <SolroutePlace key={e} />;
+        })}
         <div className='pt-24 pb-48 px-16'>
           <SolroutePlaceAddButton />
         </div>
