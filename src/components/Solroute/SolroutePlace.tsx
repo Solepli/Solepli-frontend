1a82b39694780997b618cde87b4f232384f03632
ion <delion.dev@gmail.com>
2025-07-01 22:01:02 +0900

    [feat/#172] SolroutePlace 우측 장소 정보 및 삭제 UI 구현

--- src/components/Solroute/SolroutePlace.tsx
+++ src/components/Solroute/SolroutePlace.tsx
@@ -1,19 +1,47 @@
 import DragAndDropLine from '../../assets/dragAndDropLine.svg?react';
+import Trash from '../../assets/trash.svg?react';
 
 const SolroutePlace = () => {
   return (
     <div className='flex items-start self-stretch'>
-      <div className='flex pl-20 items-start gap-16 flex-[1_0_0]'>
+      <div className='flex pl-20 items-start gap-16 grow'>
+        {/* line */}
         <div className='flex w-9 flex-col justify-center items-center gap-4 self-stretch'>
           <img src={'/solrouteLineDash.svg'} alt='SolrouteLineDash' />
-          <div className='flex w-24 h-24 flex-col justify-center items-center gap-10 aspect-square rounded-[8px] bg-secondary-700'>
+          <div className='flex w-24 h-24 flex-col justify-center items-center gap-10 aspect-square rounded-lg bg-secondary-700'>
             <DragAndDropLine />
           </div>
           <div
             className={`min-h-111 self-stretch bg-repeat-y bg-top bg-[url("/solrouteLineDash.svg")]`}
           />
         </div>
-        <div></div>
+
+        {/* info */}
+        <div className='flex py-12 pr-16 flex-col justify-center items-start gap-8 grow'>
+          {/* place */}
+          <div className='flex items-center gap-10 self-stretch'>
+            <div className='flex flex-col items-start gap-2 grow'>
+              <div className='flex justify-center items-center gap-8'>
+                <div className='text-primary-950 text-center text-base not-italic font-bold leading-24 tracking-tight'>
+                  성수까망
+                </div>
+                <div className='text-primary-400 text-xs not-italic font-normal leading-18 tracking-[-0.18px]'>
+                  카페
+                </div>
+              </div>
+              <div className='text-primary-400 text-sm not-italic font-normal leading-[120%] tracking-[-0.21px]'>
+                서울 성동구 연무장5가길 20 2층
+              </div>
+            </div>
+            <div className='flex w-40 h-40 justify-end items-center gap-10'>
+              <div className='flex p-4 items-center rounded-lg border-1 border-solid border-primary-700'>
+                <Trash className='text-primary-700' />
+              </div>
+            </div>
+          </div>
+
+          {/* memo */}
+        </div>
       </div>
     </div>
   );
