6cf73dd1fe2de7b3108652a06b29c4db09317976
sky121016 <sky121016@naver.com>
2025-07-02 22:25:16 +0900

    [refactor/#176] sollect Store에 sollects 추가

--- src/store/sollectStore.ts
+++ src/store/sollectStore.ts
@@ -1,13 +1,18 @@
 import { create } from 'zustand';
+import { SollectPhotoType } from '../types';
 
 interface SollectStore {
   selectedCategory: string;
+  sollects: SollectPhotoType[];
   setSelectedCategory: (category: string) => void;
   clearCategory: () => void;
+  setSollects: (sollects: SollectPhotoType[]) => void;
 }
 
 export const useSollectStore = create<SollectStore>((set) => ({
   selectedCategory: '',
+  sollects: [],
   setSelectedCategory: (category) => set({ selectedCategory: category }),
   clearCategory: () => set({ selectedCategory: '' }),
+  setSollects: (sollects) => set({ sollects: sollects }),
 }));
