ba042bced0eeea5df79a00855ef3fa2b557ff98e
lth-1026 <th20001026@gmail.com>
2025-07-03 19:58:15 +0900

    [fix/#152] TitleHeader 가장 위로 고정되도록 top-0 추가

--- src/components/global/TitleHeader.tsx
+++ src/components/global/TitleHeader.tsx
@@ -5,7 +5,7 @@ import { TitleHeaderProps } from '../../interface';
 const TitleHeader: React.FC<TitleHeaderProps> = ({ title, onClick, center }) => {
   return (
     <header
-      className={`w-full flex items-center py-8 fixed z-10 bg-white ${center ? 'text-center' : ''}`}>
+      className={`w-full flex items-center py-8 fixed z-10 bg-white top-0 ${center ? 'text-center' : ''}`}>
       <img
         src={arrow}
         alt='arrow'
