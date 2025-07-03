01a8f65a660bbc3a3b0c37cf8668f030e560c2b3
lth-1026 <th20001026@gmail.com>
2025-07-03 19:57:18 +0900

    [feat/#152] ReviewWriteTriggerEmoji에서도 pt 정할 수 있도록 수정

--- src/components/BottomSheet/ReviewWrite/ReviewWriteTriggerEmoji.tsx
+++ src/components/BottomSheet/ReviewWrite/ReviewWriteTriggerEmoji.tsx
@@ -5,11 +5,13 @@ import ReviewEmoji from './ReviewEmoji';
 interface ReviewWriteTriggerEmojiProps {
   placeId: number;
   placeName: string;
+  pt?: 20 | 40;
 }
 
 const ReviewWriteTriggerEmoji: React.FC<ReviewWriteTriggerEmojiProps> = ({
   placeId,
   placeName,
+  pt=40,
 }) => {
   //로그인을 안했다면 로그인을 요청한 후 리뷰 작성 page로 넘어감
   const navigate = useNavigate();
@@ -21,7 +23,7 @@ const ReviewWriteTriggerEmoji: React.FC<ReviewWriteTriggerEmojiProps> = ({
 
   return (
     <LoginRequiredAction onAction={handleEmojiClick}>
-      <ReviewEmoji />
+      <ReviewEmoji pt={pt} />
     </LoginRequiredAction>
   );
 };
