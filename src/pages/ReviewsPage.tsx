da4b108af9a54241c20117e5a20f5d68a3fe7ebe
lth-1026 <th20001026@gmail.com>
2025-07-03 20:08:42 +0900

    [feat/#152] 전체 review 조회하는 page 구현
    infiniteQuery로 review api를 호출함.
    
    getNextPageParam 함수를 통해 마지막으로 호출한 api의 nextCursor 값을 반환해 hasNextPage의 값을 설정함.
    
    useScrollSentinel hook을 이용해 추가적으로 fetching할지 판단 및 실행함.

--- src/pages/ReviewsPage.tsx
+++ src/pages/ReviewsPage.tsx
@@ -1,14 +1,66 @@
-import { useLocation, useParams } from 'react-router-dom';
-import ReviewList from '../components/BottomSheet/Review/ReviewList';
+import { useNavigate, useParams } from 'react-router-dom';
+import { usePlaceStore } from '../store/placeStore';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { getReviews } from '../api/reviewApi';
+import { useScrollSentinel } from '../hooks/useInfiniteScrollQuery';
+import Review from '../components/BottomSheet/Review/Review';
+import TitleHeader from '../components/global/TitleHeader';
+import ReviewWriteTriggerEmoji from '../components/BottomSheet/ReviewWrite/ReviewWriteTriggerEmoji';
 
 const ReviewsPage = () => {
   const { placeId } = useParams<{ placeId: string }>();
-  const location = useLocation();
-  const placeName = location.state?.placeName || '';
+  //TODO:: 장소 이름 api 받아서 사용할 것
+  const { selectedPlace } = usePlaceStore();
+  const navigate = useNavigate();
 
-  if (!placeId) return <div>잘못된 접근입니다.</div>;
+  // useInfiniteQuery를 통한 무한 스크롤 구현
+  // pageParam이 cursorId 역할을 한다. 초기값은 undefined
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery({
+      queryKey: ['getAllReviews'],
+      queryFn: ({ pageParam = undefined }) =>
+        getReviews(parseInt(placeId ?? '1'), pageParam),
+      getNextPageParam: (lastPage) => {
+        return lastPage.nextCursor;
+      },
+      initialPageParam: undefined,
+    });
 
-  return <ReviewList placeId={parseInt(placeId)} placeName={placeName} showAll />;
+  // 데이터 합침
+  const reviews = data ? data.pages.map((page) => page.reviews).flat() : [];
+
+  // sentinelRef div가 뷰포트에 들어오면 다음 페이지 fetch
+  // 스크롤 끝까지 했는지 sentinel 감시
+  const sentinelRef = useScrollSentinel({
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  });
+
+  return (
+    <div className='relative'>
+      <TitleHeader
+        title={selectedPlace?.name ?? ''}
+        onClick={() => {
+          navigate(`/map/detail/${placeId}`);
+        }}
+      />
+      <div className='mt-58 mb-32'>
+        <ReviewWriteTriggerEmoji
+          placeId={parseInt(placeId ?? '')}
+          placeName={selectedPlace?.name ?? ''}
+          pt={20}
+        />
+        {reviews.map((review) => (
+          <Review review={review} key={review.nickName + review.createdAt} />
+        ))}
+        <div ref={sentinelRef} style={{ height: '1px' }} />
+        {isFetchingNextPage && (
+          <div className='text-center py-4'>로딩 중...</div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ReviewsPage;
