80119d2eaff10a3b60d494caa941c400ec08cc2f
lth-1026 <th20001026@gmail.com>
2025-07-04 12:05:46 +0900

    [refactor/#152] FilePicker로 리뷰 이미지를 추가

--- src/components/BottomSheet/ReviewWrite/ReviewPhotosInput.tsx
+++ src/components/BottomSheet/ReviewWrite/ReviewPhotosInput.tsx
@@ -1,11 +1,23 @@
-import { useRef } from 'react';
+import React from 'react';
 import Photo from '../../../assets/photoAddIcon.svg?react';
 
 import useReviewWriteStore from '../../../store/reviewWriteStore';
 import { useShallow } from 'zustand/shallow';
 import ReviewPhotos from '../Review/ReviewPhotos';
+import FilePicker from '../../global/FilePicker';
 
-const MAX_FILE_SIZE: number = Number(import.meta.env.VITE_MAX_FILE_SIZE);
+const AddPhoto: React.FC<{
+  onClick: React.MouseEventHandler<HTMLDivElement>;
+}> = ({ onClick }) => {
+  return (
+    <div onClick={onClick} className='flex py-4 gap-2'>
+      <Photo />
+      <button className='pl-2 text-sm leading-tight text-primary-500 font-normal whitespace-nowrap'>
+        사진 추가하기
+      </button>
+    </div>
+  );
+};
 
 const ReviewPhotosInput = () => {
   const { files, setFiles } = useReviewWriteStore(
@@ -14,32 +26,6 @@ const ReviewPhotosInput = () => {
       setFiles: state.setFiles,
     }))
   );
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const handleClick = () => {
-    fileInputRef.current?.click();
-  };
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let selectedFiles = Array.from(event.target.files || []);
-    console.log('Selected files:', selectedFiles);
-    if (!selectedFiles) return;
-
-    // Check if the number of selected files exceeds 5
-    if (files.length + selectedFiles.length > 5) {
-      alert('사진은 최대 5장까지 추가할 수 있습니다.');
-      selectedFiles = Array.from(selectedFiles).slice(0, 5 - files.length);
-    }
-
-    selectedFiles = selectedFiles.filter((file) => {
-      if (file.size > MAX_FILE_SIZE) {
-        alert('파일 크기는 5MB를 초과할 수 없습니다.');
-        return false;
-      }
-      return true;
-    });
-    setFiles([...files, ...selectedFiles]);
-  };
 
   const handleDelete = (index: number) => {
     setFiles(files.filter((_, i) => i !== index));
@@ -53,21 +39,10 @@ const ReviewPhotosInput = () => {
           onDeleteFunc={handleDelete}
         />
       </div>
-      <div
-        className='flex flex-row items-center gap-4 py-4 px-20'
-        onClick={handleClick}>
-        <Photo />
-        <button className='text-[12px] leading-[120%] tracking-[-0.18px] text-primary-500 whitespace-nowrap'>
-          사진 추가하기
-        </button>
-        <input
-          type='file'
-          accept='.png, .jpg, .jpeg'
-          multiple
-          ref={fileInputRef}
-          onChange={handleFileChange}
-          className='hidden'
-        />
+      <div className='pl-20'>
+        <FilePicker files={files} onChange={(files) => setFiles(files)}>
+          {(open) => <AddPhoto onClick={open} />}
+        </FilePicker>
       </div>
     </div>
   );
