540d0efc468b490fb2c15d4494d10135c7c41b05
lth-1026 <th20001026@gmail.com>
2025-07-05 17:42:00 +0900

    [refactor/#183] utils/icon을 이용해 Icon 구현

--- src/components/Searching/RelatedSearchPlace.tsx
+++ src/components/Searching/RelatedSearchPlace.tsx
@@ -1,40 +1,20 @@
 import React from 'react';
-import food from '../../assets/category-icons/foodFill.svg';
-import cafe from '../../assets/category-icons/cafeFill.svg';
-import drink from '../../assets/category-icons/drinkFill.svg';
-import entertainment from '../../assets/category-icons/entertainmentFill.svg';
-import culture from '../../assets/category-icons/cultureFill.svg';
-import shop from '../../assets/category-icons/shopFill.svg';
-import walk from '../../assets/category-icons/walkFill.svg';
-import work from '../../assets/category-icons/workFill.svg';
-import location from '../../assets/locationFill.svg';
 import type { PlaceInfo } from '../../types';
 import SelectableChip from '../global/SelectableChip';
-
-const iconMap: Record<string, string> = {
-  food,
-  cafe,
-  drink,
-  entertainment,
-  culture,
-  shop,
-  walk,
-  work,
-  location,
-};
+import { iconRelatedSearch } from '../../utils/icon';
 
 interface RelatedSearchPlaceProps {
   place: PlaceInfo;
 }
 
 const RelatedSearchPlace: React.FC<RelatedSearchPlaceProps> = ({ place }) => {
-  const icon = iconMap[place.category!];
+  const Icon = iconRelatedSearch[place.category!];
 
   return (
     <div className='flex p-[16px_16px_4px_16px] items-center gap-10 self-stretch'>
       {/* 아이콘 */}
       <div className='flex p-4 items-start rounded-[4px] bg-gray-400/10'>
-        <img className='w-24 h-24' src={icon} alt={place.category} />
+        <Icon />
       </div>
 
       {/* 이름 */}
