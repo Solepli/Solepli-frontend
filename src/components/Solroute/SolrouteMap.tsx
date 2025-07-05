d03423a0b1196c7102c5c3c899997f11b7cd3351
ion <delion.dev@gmail.com>
2025-07-05 16:04:48 +0900

    [refactor/#181] SolrouteMap에서 사용하지 않는 expandBounds 함수 제거

--- src/components/Solroute/SolrouteMap.tsx
+++ src/components/Solroute/SolrouteMap.tsx
@@ -4,7 +4,6 @@ import {
   createMarkerObjectList,
   createMarkersBounds,
   deleteMarkers,
-  expandBounds,
   initMap,
 } from '../../utils/mapFunc';
 import { useSolrouteWriteStore } from '../../store/solrouteWriteStore';
