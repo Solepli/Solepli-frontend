610f07a8e072710fc8f319f05160adca57a3b2ba
sky121016 <sky121016@naver.com>
2025-07-06 17:38:24 +0900

    [refactor/#197] 스타일 수정

--- src/components/Sollect/SollectDetail/AddCourseButton.tsx
+++ src/components/Sollect/SollectDetail/AddCourseButton.tsx
@@ -8,15 +8,15 @@ const AddCourseButton = () => {
     setIsAdded(!isAdded);
     // add to course
   };
-
+  const style = "rounded-full border-1 border-primary-700 py-4 pr-16 pl-8 flex text-sm font-bold items-center"
   return (
     <div onClick={clickButton}>
       {isAdded ? (
-        <button className='bg-primary-700 rounded-full border-1 border-primary-700 py-5 text-white pr-16 pl-4 flex text-sm font-bold items-center'>
+        <button className={'bg-primary-700  text-white' + style}>
           <img src={check} alt='check' className='w-24 h-24' /> 코스로 저장
         </button>
       ) : (
-        <button className='bg-white rounded-full border-1 border-primary-700 py-5 text-primary-700 pr-16 pl-4 flex text-sm font-bold items-center'>
+        <button className={'bg-white  text-primary-700 ' + style}>
           <img src={addBlack} alt='add' className='w-24 h-24' /> 코스로 저장
         </button>
       )}
