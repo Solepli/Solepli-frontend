319001e3fbd8bcdde8186e6eefb318cda3f9598f
ion <delion.dev@gmail.com>
2025-07-08 04:54:47 +0900

    [fix/#195] mapApi 및 searchApi privateAxios로 수정

--- src/api/mapApi.ts
+++ src/api/mapApi.ts
@@ -1,5 +1,5 @@
 import qs from 'qs';
-import { publicAxios } from './axios';
+import { privateAxios } from './axios';
 import { ENDPOINT } from './urls';
 
 export const getMarkersByDisplay = async (
@@ -9,7 +9,7 @@ export const getMarkersByDisplay = async (
   neLng: number, // neX
   category?: string
 ) => {
-  const response = await publicAxios.get(ENDPOINT.SOLMAP_MARKERS, {
+  const response = await privateAxios.get(ENDPOINT.SOLMAP_MARKERS, {
     params: {
       swLat,
       swLng,
@@ -23,13 +23,15 @@ export const getMarkersByDisplay = async (
 };
 
 export const getMarkersByRegion = async (regionName: string) => {
-  const res = await publicAxios.get(ENDPOINT.SOLMAP_MARKERS_REGION(regionName));
+  const res = await privateAxios.get(
+    ENDPOINT.SOLMAP_MARKERS_REGION(regionName)
+  );
 
   return res.data.data;
 };
 
 export const getMarkersByIdList = async (ids: number[]) => {
-  const res = await publicAxios.get(ENDPOINT.SOLMAP_MARKERS_ID_LIST, {
+  const res = await privateAxios.get(ENDPOINT.SOLMAP_MARKERS_ID_LIST, {
     params: { ids },
     paramsSerializer: (params) =>
       qs.stringify(params, { arrayFormat: 'repeat' }),
