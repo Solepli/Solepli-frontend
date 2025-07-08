0cf2844d49165adca75c9e863030c5f1ab0e8b98
lth-1026 <th20001026@gmail.com>
2025-07-09 04:34:12 +0900

    [fix/#218] 쏠렉트 작성시 100장 이상 이미지 첨부 문제 해결
    쏠렉트 이미지 추가 로직이 단순 file 배열에 file을 추가하지 않아서 발생한 문제.
    쏠렉트 이미지 추가 함수는 기존 파일 배열에 새로운 이미지를 추가함.
    이에 FilePicker에 외부 함수에 의해서만 관리되는 경우를 managedExternally로 추가함

--- src/components/Sollect/SollectWrite/SollectWriteImageInput.tsx
+++ src/components/Sollect/SollectWrite/SollectWriteImageInput.tsx
@@ -4,13 +4,14 @@ import { useShallow } from 'zustand/shallow';
 import { useSollectWriteStore } from '../../../store/sollectWriteStore';
 
 const SollectWriteImageInput = () => {
-  const { paragraphs, focusTextarea, insertImageAtCaret } = useSollectWriteStore(
-    useShallow((state) => ({
-      paragraphs: state.paragraphs,
-      focusTextarea: state.focusTextarea,
-      insertImageAtCaret: state.insertImageAtCaret,
-    }))
-  );
+  const { paragraphs, focusTextarea, insertImageAtCaret } =
+    useSollectWriteStore(
+      useShallow((state) => ({
+        paragraphs: state.paragraphs,
+        focusTextarea: state.focusTextarea,
+        insertImageAtCaret: state.insertImageAtCaret,
+      }))
+    );
 
   const onFileChange = (newFiles: File[]) => {
     if (newFiles.length === 0) return;
@@ -20,17 +21,20 @@ const SollectWriteImageInput = () => {
     }
     newFiles.reverse().forEach((file) => {
       insertImageAtCaret(file, caret);
-    })
+    });
   };
 
   return (
     <div className='w-full h-44 flex items-center justify-start px-16 fixed bottom-0 border-t border-grayScale-100 bg-white'>
       <FilePicker
-        files={paragraphs.filter((p) => p.type === 'IMAGE').map((p) => p.file).filter((file): file is File => file !== undefined)}
+        files={paragraphs
+          .filter((p) => p.type === 'IMAGE')
+          .map((p) => p.file)
+          .filter((file): file is File => file !== undefined)}
         onChange={onFileChange}
         multiple={true}
         maxCount={100}
-        keepFiles={false}>
+        managedExternally={true}>
         {(open) => <AddLogo onClick={open} />}
       </FilePicker>
     </div>
