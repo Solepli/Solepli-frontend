0320c317afb3c17a847073cae04f7dc51db96ec5
sky121016 <sky121016@naver.com>
2025-07-08 19:26:34 +0900

    [refactor/#200] 스타일 수정

--- src/components/BottomSheet/Preview/PreviewContentEmpty.tsx
+++ src/components/BottomSheet/Preview/PreviewContentEmpty.tsx
@@ -28,10 +28,10 @@ const PreviewContentEmpty: React.FC = () => {
     <div>
       <div className='flex py-80 flex-col items-start'>
         <div className='flex px-16 flex-col justify-center items-center gap-4 self-stretch'>
-          <div className='text-base font-bold leading-[120%] tracking-[-0.4px] text-center text-primary-900'>
+          <div className='font-bold leading-[120%] tracking-[-0.4px] text-center text-primary-900'>
             검색 결과를 찾지 못 했어요
           </div>
-          <div>검색어를 다시 입력해주세요</div>
+          <div className='text-xs text-primary-900'>검색어를 다시 입력해주세요</div>
         </div>
       </div>
       <div className='flex pt-0 px-16 pb-16 items-center gap-10 self-stretch'>
