609324067f0d82236015a21a0760874b59037c48
sky121016 <sky121016@naver.com>
2025-07-09 15:24:35 +0900

    [feat/#221] 회원 화면, 간편 로그인 버튼

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,13 +1,18 @@
 import { useShallow } from 'zustand/shallow';
 import useAuthStore from '../store/authStore';
 import ProfileMenu from '../components/Profile/ProfileMenu';
+import profile from '../assets/profile.svg';
+import { useNavigate } from 'react-router-dom';
 
 const Profile = () => {
+  const navigate = useNavigate();
   const { logout } = useAuthStore(
     useShallow((state) => ({
       logout: state.logout,
     }))
   );
+
+  // 유저 정보 받아오기 필요
 
   const isLoggedIn = useAuthStore(useShallow((state) => state.isLoggedIn));
   return (
@@ -15,7 +20,15 @@ const Profile = () => {
       {isLoggedIn ? (
         // 회원
         <>
-          <div className='h-260 font-semibold flex justify-center items-center text-center bg-primary-100'></div>
+          <div className='h-260 flex flex-col justify-center items-center text-center bg-primary-100'>
+            <img src={profile} alt='profile' className='mb-16' />
+            <p className='font-semibold text-primary-950 mb-4'>이름</p>
+            <p
+              className='text-sm text-primary-700 underline'
+              onClick={() => navigate('/profile/edit')}>
+              프로필 수정
+            </p>
+          </div>
         </>
       ) : (
         // 비회원
@@ -26,10 +39,11 @@ const Profile = () => {
               <p>더 많은 정보를 얻을 수 있습니다!</p>
             </div>
           </div>
-
         </>
       )}
-      <ProfileMenu isLoggedIn={isLoggedIn}/>
+      <ProfileMenu isLoggedIn={isLoggedIn} />
+
+     
 
       {/* <h1>Sollect</h1>
       <button onClick={logout}>로그아웃</button>
