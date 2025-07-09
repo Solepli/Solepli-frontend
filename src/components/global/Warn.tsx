12402241fa638999a15d6b7fee483a6e404e3a0a
lth-1026 <th20001026@gmail.com>
2025-07-09 18:02:05 +0900

    [feat/#219] 토스트 좌우 여백 16px 제외 가로 최대한 늘림

--- src/components/global/Warn.tsx
+++ src/components/global/Warn.tsx
@@ -1,14 +1,21 @@
 import WarnIcon from '../../assets/warn.svg?react';
-const Warn: React.FC<{ title: string; message?: string }> = ({ title, message }) => {
+const Warn: React.FC<{ title: string; message?: string }> = ({
+  title,
+  message,
+}) => {
   return (
-    <div className='flex w-361 px-12 py-8 bg-primary-950 rounded-lg gap-4 items-center'>
-        <WarnIcon />
-        <div className='flex-1 flex flex-col gap-2'>
-            <span className='text-primary-50 text-sm font-semibold leading-tight'>{title}</span>
-            {message && (
-                <span className='text-primary-50 text-xs font-normal leading-none'>{message}</span>
-            )}
-        </div>
+    <div className='flex w-full px-12 py-8 bg-primary-950 rounded-lg gap-4 items-center'>
+      <WarnIcon className='min-w-24' />
+      <div className='flex flex flex-col gap-2'>
+        <span className='text-primary-50 text-sm font-semibold leading-tight'>
+          {title}
+        </span>
+        {message && (
+          <span className='text-primary-50 text-xs font-normal leading-none'>
+            {message}
+          </span>
+        )}
+      </div>
     </div>
   );
 };
