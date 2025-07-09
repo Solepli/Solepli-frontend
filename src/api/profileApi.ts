e9b62fe262e695dd699b8f2e71493182931f7452
sky121016 <sky121016@naver.com>
2025-07-09 20:45:48 +0900

    [feat/#221] 프로필 수정 구현

--- src/api/profileApi.ts
+++ src/api/profileApi.ts
@@ -9,3 +9,25 @@ export const fetchProfile = async () => {
     console.log(e);
   }
 };
+
+export const patchProfile = async (
+  profileData: { nickname: string },
+  imageFile?: File
+) => {
+  const formData = new FormData();
+  const jsonBlob = new Blob([JSON.stringify(profileData)], {
+    type: 'application/json',
+  });
+  formData.append('request', jsonBlob);
+
+  if (imageFile) {
+    formData.append('file', imageFile);
+  }
+
+  try {
+    const res = await privateAxios.patch(ENDPOINT.PROFILE, formData);
+    return res.data.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
