ae1ed21a9e1b5aaeaa904626d19b2fbf12106bfe
sky121016 <sky121016@naver.com>
2025-07-09 22:09:01 +0900

    [fix/#221] 쏠렉트 포토 너비 수정

--- src/components/Sollect/SollectList.tsx
+++ src/components/Sollect/SollectList.tsx
@@ -15,7 +15,7 @@ const SollectList: React.FC<SollectListProps> = ({
   if (horizontal) {
     style += ' px-16 flex overflow-x-scroll flex-row gap-4 overflow-y-auto';
   } else {
-    style += ' px-16 gap-12 overflow-y-auto grid grid-cols-2';
+    style += ' px-16 gap-12 overflow-y-auto grid grid-cols-2 w-full w-max-100vw';
   }
 
   return (
