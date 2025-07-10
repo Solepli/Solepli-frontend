4f32685db79ea61806796fa205439b71a0fa7061
lth-1026 <th20001026@gmail.com>
2025-07-10 21:00:30 +0900

    [feat/#232] 쏠렉트 이미지 추가 하단 바 키보드 위로 고정
    현재 100% 작동하지 않음.
    추후 개선 필요

--- src/pages/SollectWritePage.tsx
+++ src/pages/SollectWritePage.tsx
@@ -1,11 +1,15 @@
+import { useRef } from 'react';
 import SollectWriteContent from '../components/Sollect/SollectWrite/SollectWriteContent';
 import SollectWriteImageInput from '../components/Sollect/SollectWrite/SollectWriteImageInput';
 
 const SollectWritePage = () => {
+  const footerRef = useRef<HTMLDivElement>(null);
   return (
     <div className='w-full h-full flex flex-col relative overflow-hidden'>
-      <SollectWriteContent />
-      <SollectWriteImageInput />
+      <SollectWriteContent footerRef={footerRef} />
+      <div>
+        <SollectWriteImageInput footerRef={footerRef} />
+      </div>
     </div>
   );
 };
