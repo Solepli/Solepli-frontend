51ee5a4fbe003c7ed03510d3a665cb0f471e9460
sky121016 <sky121016@naver.com>
2025-07-11 19:53:45 +0900

    [feat/#258] 쏠마크 쏠렉트 무한스크롤

--- src/components/Solmark/SolmarkContentSollect.tsx
+++ src/components/Solmark/SolmarkContentSollect.tsx
@@ -1,33 +1,57 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { useEffect } from 'react';
 import { fetchSolmarkSollect } from '../../api/solmarkApi';
 import SollectList from '../Sollect/SollectList';
 import SolmarkNoResult from './SolmarkNoResult';
 import { useSollectStore } from '../../store/sollectStore';
 import { SollectPhotoType } from '../../types';
+import { useScrollSentinel } from '../../hooks/useInfiniteScrollQuery';
 
 const SolmarkContentSollect = () => {
-  const { data } = useQuery({
-    queryKey: ['solmarkSollects'],
-    queryFn: () => fetchSolmarkSollect(),
-  });
 
   const { sollects, setSollects } = useSollectStore();
 
+
+
+
+   const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+     useInfiniteQuery({
+       queryKey: ['solmarkSollects'],
+       queryFn: ({ pageParam = undefined }) => fetchSolmarkSollect(pageParam),
+       getNextPageParam: (lastPage) => {
+         if (!lastPage?.cursorInfo) return undefined;
+         return lastPage?.cursorInfo.nextCursorId;
+       },
+       initialPageParam: undefined,
+     });
+
   useEffect(() => {
-    if (data) {
-      const markedSollects = data.map((sollect:SollectPhotoType) => ({
-        ...sollect,
-        isMarked: true,
-      }));
-      setSollects(markedSollects);
-    }
+    // 데이터 합침
+    const flattened =
+      data?.pages.flatMap((page) =>
+        page.contents.map((sollect: SollectPhotoType) => ({
+          ...sollect,
+          isMarked: true,
+        }))
+      ) ?? [];
+    setSollects(flattened);
   }, [data, setSollects]);
+  
+  const sentinelRef = useScrollSentinel({
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  });
+
+
 
   return (
     <div className='py-16'>
       {sollects.length !== 0 ? (
-        <SollectList />
+        <div>
+          <SollectList />
+          <div ref={sentinelRef} style={{ height: '1px' }} />
+        </div>
       ) : (
         <SolmarkNoResult type='sollect' />
       )}
