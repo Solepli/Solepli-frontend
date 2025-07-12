8ceef95931a8fb3d28624c66cea6f1fbdbe8d425
lth-1026 <th20001026@gmail.com>
2025-07-12 14:15:06 +0900

    [feat/#276] 아이콘 모달 선택시 모당 창 닫힘

--- src/components/global/selectableIconSet/SelectableIconSet.tsx
+++ src/components/global/selectableIconSet/SelectableIconSet.tsx
@@ -3,9 +3,10 @@ import { selectableIconMap } from '../../../utils/icon';
 
 interface SelectableIconSetProps {
   setIcon: (iconKey: number) => void;
+  closeModal: (turn: boolean) => void;
 }
 
-const SelectableIconSet: React.FC<SelectableIconSetProps> = ({ setIcon }) => {
+const SelectableIconSet: React.FC<SelectableIconSetProps> = ({ setIcon, closeModal }) => {
   const onClickIcon = (iconKey: string) => {
     const iconNumber = parseInt(iconKey);
     setIcon(iconNumber);
@@ -13,13 +14,12 @@ const SelectableIconSet: React.FC<SelectableIconSetProps> = ({ setIcon }) => {
   return (
     <div
       className='w-373 h-124 p-16 grid grid-cols-7 gap-y-12 gap-x-10 justify-items-center bg-white rounded-lg 
-    shadow-[3px_3px_4px_0px_rgba(24,24,24,0.06)] outline outline-1 outline-offset-[-1px] outline-primary-100'
-      onClick={(e) => e.stopPropagation()}>
+    shadow-[3px_3px_4px_0px_rgba(24,24,24,0.06)] outline outline-1 outline-offset-[-1px] outline-primary-100'>
       {Object.entries(selectableIconMap).map(([key, Icon]) => (
         <div
           key={key}
           className='w-40 h-40 flex items-center justify-center bg-primary-50 rounded outline outline-1 outline-offset-[-1px] outline-primary-100'
-          onClick={() => onClickIcon(key)}>
+          onClick={() => {closeModal(false); onClickIcon(key)}}>
           <Icon />
         </div>
       ))}
