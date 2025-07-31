7d4628f0f6d75585d013a830bc97a053612d3a27
lth-1026 <th20001026@gmail.com>
2025-07-31 23:47:06 +0900

    [refactor/#300] 쏠렉트 하단바 키보드 위에 고정
    ios의 safari에선 키보드가 닫힐 때 blur 이벤트가 즉각 반응하고
    chrome에선 visualViewport의 resize 이벤트가 즉각 반응함.
    이를 이용해 하단바를 키보드 위에 고정시킬 수 있었음.
    
    다만 핸드폰 성능에 따라 하단바가 키보드 위로 올라오는 속도가 다름
    아이폰 12까지는 문제 없으나
    아이폰 11이하부터는 delay가 있음

--- src/pages/SollectWritePage.tsx
+++ src/pages/SollectWritePage.tsx
@@ -3,13 +3,47 @@ import SollectWriteContent from '../components/Sollect/SollectWrite/SollectWrite
 import SollectWriteImageInput from '../components/Sollect/SollectWrite/SollectWriteImageInput';
 
 const SollectWritePage = () => {
-  const footerRef = useRef<HTMLDivElement>(null);
+  const footerRef = useRef<HTMLDivElement | null>(null);
+  const contentRef = useRef<HTMLDivElement | null>(null);
+  //setTimeout을 이용해 항상 footer의 input Event가 먼저 반응할 수 있도록 함
+  const vv = window.visualViewport;
+  if (!vv) return <div>Visual viewport not supported</div>;
+
+  const fullHeight = vv.height;
+  // viewport가 변할 때 footer 위치 조정
+  // chrome에서는 resize 이벤트가 즉시 반응함
+  vv.addEventListener('resize', () => {
+    setTimeout(() => {
+      const contentHeight = contentRef.current;
+      if (!contentHeight) return;
+      if (vv.height >= fullHeight - 10) {
+        contentHeight.style.height = `100dvh`; // Adjust footer position based on viewport height
+      } else {
+        // header와 footer를 제외한 영역의 높이를 조정
+        contentHeight.style.height = `${vv.height - 95}px`; // Adjust footer position based on viewport height
+        window.scrollTo(0, 1); // Scroll to the top to avoid keyboard overlap
+      }
+    }, 0);
+  });
+
+  //textarea focus/blur 시 footer 위치 조정
+  //safari에서는 blur 이벤트가 즉시 반응함
+  function focus() {
+    setTimeout(() => {
+      console.log('focus timeout');
+      window.scrollTo(0, 1); // Scroll to the top to avoid keyboard overlap
+      contentRef.current!.style.height = `${vv!.height - 95}px`; // Adjust height based on viewport
+    }, 0);
+  }
+  function blur() {
+    setTimeout(() => {
+      contentRef.current!.style.height = `100dvh`; // Adjust height based on viewport
+    }, 0);
+  }
   return (
-    <div className='w-full h-full flex flex-col relative overflow-hidden'>
-      <SollectWriteContent footerRef={footerRef} />
-      <div>
-        <SollectWriteImageInput footerRef={footerRef} />
-      </div>
+    <div className='w-full h-full flex flex-col'>
+      <SollectWriteContent footerRef={footerRef} contentRef={contentRef} />
+      <SollectWriteImageInput footerRef={footerRef} />
     </div>
   );
 };
