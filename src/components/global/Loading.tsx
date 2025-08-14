60bffcc0b5ebb4ef75e15f74de57849ae6a36191
lth-1026 <th20001026@gmail.com>
2025-08-14 23:34:05 +0900

    [refactor/#330] Loading 컴포넌트에 delay 기능 추가
    response가 0.5초 이내로 왔을 때 그 사이에도 Loading 컴포넌트가 나타나면 사용자가 피곤함을 느낌.
    이를 방지하기 위해 delay를 걸어 delay 이내로 response가 오면 Loading 컴포넌트가 나타나지 않음

--- src/components/global/Loading.tsx
+++ src/components/global/Loading.tsx
@@ -1,8 +1,46 @@
+import { useEffect, useState } from 'react';
 import Logo from '../../assets/logo.svg?react';
 
-const Loading = ({ text }: { text: string }) => {
+/*
+  response가 0.5초 이내로 왔을 때 그 사이에도 Loading 컴포넌트가 나타나면 사용자가 피곤함을 느낌.
+  이를 방지하기 위해 delay를 추가함.
+  delay 이내로 response가 오면 Loading 컴포넌트가 나타나지 않음
+
+  text 뒤 자동으로 애니메이션이 추가된 ...을 붙여 진행 중임을 나타냄
+*/
+
+const Loading = ({
+  text,
+  active,
+  delayMs = 500,
+}: {
+  text: string;
+  active: boolean;
+  delayMs?: number; //delay 시간 (ms)
+}) => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout> | null = null;
+
+    if (active) {
+      timer = setTimeout(() => {
+        setShow(true);
+      }, delayMs);
+    } else {
+      if (timer) clearTimeout(timer);
+      setShow(false);
+    }
+
+    return () => {
+      if (timer) clearTimeout(timer);
+    };
+  }, [active, delayMs]);
+
+  if (!show) return null;
+
   return (
-    <div className='absolute inset-0 bg-black/80 flex justify-center items-center'>
+    <div className='fixed inset-0 bg-black/80 flex justify-center items-center z-500'>
       <div className='w-160 h-200 rounded-lg bg-white flex flex-col justify-center items-center gap-24'>
         <Logo className='w-50 h-50 animate-breathe' />
         <p className='text-primary-950 text-sm'>
