9e196451ec988daf9544ec2bc3e16ecf2dc85ae6
DandelionQZ <delion.dev@gmail.com>
2025-08-20 22:08:40 +0900

    [feat/#339] 쏠맵 랜딩페이지 카테고리 바텀시트에 추천 장소 리스트 컴포넌트 추가
    CategoryButtonList 컴포넌트에서 추천 장소를 가져와 RecommendedPreviewContentList를 조건부로 렌더링하도록 수정

--- src/components/BottomSheet/Category/CategoryButtonList.tsx
+++ src/components/BottomSheet/Category/CategoryButtonList.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import CategoryButton from './CategoryButton';
 import { categories } from '../../../utils/category';
+import { usePlaceStore } from '../../../store/placeStore';
+import RecommendedPreviewContentList from '../Preview/RecommendedPreviewContentList';
 
 const CategoryButtonList: React.FC = () => {
+  const { recommendedPlaces } = usePlaceStore();
 
   return (
-    <div className='p-16 pt-0'>
-      <h1 className='pl-4 py-12 text-black text-lg font-bold'>카테고리</h1>
-      <div className='flex flex-wrap justify-between gap-y-8'>
-        {categories.map((category) => {
-          return <CategoryButton category={category} key={category.id} />;
-        })}
+    <div>
+      <div className='px-16 pb-16'>
+        <h1 className='pl-4 py-12 text-black text-lg font-bold'>카테고리</h1>
+        <div className='flex flex-wrap justify-between gap-y-8'>
+          {categories.map((category) => {
+            return <CategoryButton category={category} key={category.id} />;
+          })}
+        </div>
       </div>
+
+      {recommendedPlaces.length > 0 && <RecommendedPreviewContentList />}
     </div>
   );
 };
